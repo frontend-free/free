45e060c9a4886735f51ff41971a881fa6a9542a4
liyatang <liyatang@qq.com>
2025-02-27 20:57:42 +0800

    chore: changeset Version Packages

---
+++ common/vite-plugin-remove-svg-color/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @fe-free/vite-plugin-remove-svg-color
+
+## 1.3.3
+
+### Patch Changes
+
+- feat: add @fe-free/vite-plugin-remove-svg-color
---
+++ common/vite-plugin-remove-svg-color/src/index.ts
@@ -0,0 +1,21 @@
+import { createFilter } from '@rollup/pluginutils';
+
+function vitePluginRemoveSvgColor(config: { include?: string[]; exclude?: string[] }) {
+  const { include = ['/svgs'], exclude } = config;
+  const filter = createFilter(include, exclude);
+
+  return {
+    name: 'vite-plugin-remove-svg-color',
+    transform(src, id) {
+      if (filter(id) && id.endsWith('.svg?react')) {
+        const code = src.replace(/fill: "#[0-9a-fA-F]{6}"/g, 'fill: "currentColor"');
+
+        return {
+          code,
+        };
+      }
+    },
+  };
+}
+
+export default vitePluginRemoveSvgColor;
