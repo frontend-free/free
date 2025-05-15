40f85e7f53ed8441631ad11bbe437342f2a22033
liyatang <liyatang@users.noreply.github.com>
2025-05-15 01:51:55 +0000

    deploy: 8f1266dfe0e8b0c7cb8f63d866d28f901748904d

---
+++ assets/button.stories-BfJFMAQo.js
@@ -0,0 +1,19 @@
+import{L as p}from"./date-Dj_czsjA.js";import"./index-BIZMV8Eg.js";import"./index--kG7O4B3.js";import"./index-pzNd46ub.js";import"./jsx-runtime-BjgbQsUx.js";import"./index-DmOy-Ncz.js";import"./index-D2MAbzvX.js";import"./objectWithoutPropertiesLoose-BjXSgPXB.js";import"./index-JTS_7tlc.js";import"./index-BIm0odtm.js";import"./iframe-B44EyTSa.js";import"../sb-preview/runtime.js";const R={title:"@fe-free/core/LoadingButton",component:p,tags:["autodocs"]},e={args:{children:"click and resolve",onClick:()=>new Promise(o=>{setTimeout(o,1e3)})}},r={args:{children:"click and reject",onClick:()=>new Promise((o,m)=>{setTimeout(m,1e3)})}};var t,n,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    children: 'click and resolve',
+    onClick: () => {
+      return new Promise(resolve => {
+        setTimeout(resolve, 1000);
+      });
+    }
+  }
+}`,...(s=(n=e.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};var c,i,a;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: 'click and reject',
+    onClick: () => {
+      return new Promise((_, reject) => {
+        setTimeout(reject, 1000);
+      });
+    }
+  }
+}`,...(a=(i=r.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};const T=["Resolve","Reject"];export{r as Reject,e as Resolve,T as __namedExportsOrder,R as default};
---
+++ assets/crud.stories-DdUa2Zou.js
@@ -0,0 +1,148 @@
+import{j as t}from"./jsx-runtime-BjgbQsUx.js";import{P as D,_ as l,a as me,o as ye,u as Pe,F as qe,b as ve,c as je,i as ke,d as be,e as Ce,f as Re,m as W,r as ze,C as g,B as ge,g as Ie}from"./date-Dj_czsjA.js";import"./index-BIZMV8Eg.js";import"./index--kG7O4B3.js";import{_ as N}from"./index-pzNd46ub.js";import{R as Fe,r as M}from"./index-DmOy-Ncz.js";import"./index-D2MAbzvX.js";import"./objectWithoutPropertiesLoose-BjXSgPXB.js";import"./index-JTS_7tlc.js";import"./index-BIm0odtm.js";import"./iframe-B44EyTSa.js";import"../sb-preview/runtime.js";var Be=["fieldProps","unCheckedChildren","checkedChildren","proFieldProps"],Se=Fe.forwardRef(function(e,a){var n=e.fieldProps,r=e.unCheckedChildren,o=e.checkedChildren,u=e.proFieldProps,c=N(e,Be);return t.jsx(D,l({valueType:"switch",fieldProps:l({unCheckedChildren:r,checkedChildren:o},n),ref:a,valuePropName:"checked",proFieldProps:u,filedConfig:{valuePropName:"checked",ignoreWidth:!0,customLightMode:!0}},c))}),we=["fieldProps","proFieldProps"],Te=["fieldProps","proFieldProps"],U="text",Oe=function(a){var n=a.fieldProps,r=a.proFieldProps,o=N(a,we);return t.jsx(D,l({valueType:U,fieldProps:n,filedConfig:{valueType:U},proFieldProps:r},o))},De=function(a){var n=Pe(a.open||!1,{value:a.open,onChange:a.onOpenChange}),r=me(n,2),o=r[0],u=r[1];return t.jsx(qe.Item,{shouldUpdate:!0,noStyle:!0,children:function(C){var d,q=C.getFieldValue(a.name||[]);return t.jsx(ve,l(l({getPopupContainer:function(i){return i&&i.parentNode?i.parentNode:i},onOpenChange:function(i){return u(i)},content:t.jsxs("div",{style:{padding:"4px 0"},children:[(d=a.statusRender)===null||d===void 0?void 0:d.call(a,q),a.strengthText?t.jsx("div",{style:{marginTop:10},children:t.jsx("span",{children:a.strengthText})}):null]}),overlayStyle:{width:240},placement:"rightTop"},a.popoverProps),{},{open:o,children:a.children}))}})},Ue=function(a){var n=a.fieldProps,r=a.proFieldProps,o=N(a,Te),u=M.useState(!1),c=me(u,2),C=c[0],d=c[1];return n!=null&&n.statusRender&&o.name?t.jsx(De,{name:o.name,statusRender:n==null?void 0:n.statusRender,popoverProps:n==null?void 0:n.popoverProps,strengthText:n==null?void 0:n.strengthText,open:C,onOpenChange:d,children:t.jsx("div",{children:t.jsx(D,l({valueType:"password",fieldProps:l(l({},ye(n,["statusRender","popoverProps","strengthText"])),{},{onBlur:function(f){var i;n==null||(i=n.onBlur)===null||i===void 0||i.call(n,f),d(!1)},onClick:function(f){var i;n==null||(i=n.onClick)===null||i===void 0||i.call(n,f),d(!0)}}),proFieldProps:r,filedConfig:{valueType:U}},o))})}):t.jsx(D,l({valueType:"password",fieldProps:n,proFieldProps:r,filedConfig:{valueType:U}},o))},b=Oe;b.Password=Ue;b.displayName="ProFormComponent";function _e(e,a){var n=ke(e)?be:Ce;return n(e,je(a))}let j;const $=(e,a)=>{if(j)return j(e,a);const n={阿:"a",哎:"ai",安:"an",肮:"ang",凹:"ao",丷:"ba",挀:"bai",扳:"ban",邦:"bang",勹:"bao",陂:"bei",奔:"ben",伻:"beng",屄:"bi",边:"bian",灬:"biao",憋:"bie",汃:"bin",冫:"bing",癶:"bo",峬:"bu",嚓:"ca",偲:"cai",参:"can",仓:"cang",撡:"cao",冊:"ce",嵾:"cen",曽:"ceng",叉:"cha",芆:"chai",辿:"chan",伥:"chang",抄:"chao",车:"che",抻:"chen",阷:"cheng",吃:"chi",充:"chong",抽:"chou",出:"chu",欻:"chua",揣:"chuai",巛:"chuan",刅:"chuang",吹:"chui",旾:"chun",逴:"chuo",呲:"ci",匆:"cong",凑:"cou",粗:"cu",汆:"cuan",崔:"cui",邨:"cun",搓:"cuo",咑:"da",呆:"dai",丹:"dan",当:"dang",刀:"dao",嘚:"de",扥:"dun",灯:"deng",氐:"di",甸:"dian",刁:"diao",爹:"die",丁:"ding",丟:"diu",东:"dong",吺:"dou",厾:"du",耑:"duan",垖:"dui",吨:"dun",多:"duo",妸:"e",诶:"ei",奀:"en",鞥:"eng",儿:"er",发:"fa",帆:"fan",匚:"fang",飞:"fei",分:"fen",丰:"feng",覅:"fiao",仏:"fo",紑:"fou",夫:"fu",旮:"ga",侅:"gai",甘:"gan",冈:"gang",皋:"gao",戈:"ge",给:"gei",根:"gen",刯:"geng",工:"gong",勾:"gou",估:"gu",瓜:"gua",乖:"guai",关:"guan",光:"guang",归:"gui",丨:"gun",呙:"guo",哈:"ha",咍:"hai",佄:"han",夯:"hang",茠:"hao",诃:"he",黒:"hei",拫:"hen",亨:"heng",噷:"hm",叿:"hong",齁:"hou",乎:"hu",花:"hua",怀:"huai",欢:"huan",巟:"huang",灰:"hui",昏:"hun",吙:"huo",丌:"ji",加:"jia",戋:"jian",江:"jiang",艽:"jiao",阶:"jie",巾:"jin",坕:"jing",冂:"jiong",丩:"jiu",凥:"ju",姢:"juan",噘:"jue",军:"jun",咔:"ka",开:"kai",刊:"kan",忼:"kang",尻:"kao",匼:"ke",肎:"ken",劥:"keng",空:"kong",抠:"kou",扝:"ku",夸:"kua",蒯:"kuai",宽:"kuan",匡:"kuang",亏:"kui",坤:"kun",扩:"kuo",垃:"la",来:"lai",兰:"lan",啷:"lang",捞:"lao",肋:"le",勒:"lei",崚:"leng",哩:"li",俩:"lia",奁:"lian",良:"liang",撩:"liao",毟:"lie",拎:"lin",伶:"ling",溜:"liu",囖:"lo",龙:"long",瞜:"lou",噜:"lu",驴:"lv",娈:"luan",掠:"lüe",抡:"lun",罗:"luo",呣:"m",妈:"ma",埋:"mai",嫚:"man",牤:"mang",猫:"mao",嚒:"me",呅:"mei",椚:"men",擝:"meng",咪:"mi",宀:"mian",喵:"miao",乜:"mie",民:"min",名:"ming",谬:"miu",摸:"mo",哞:"mou",毪:"mu",嗯:"ńg",拏:"na",腉:"nai",囡:"nan",囔:"nang",孬:"nao",疒:"ne",娞:"nei",恁:"nen",能:"neng",妮:"ni",拈:"nian",娘:"niang",鸟:"niao",捏:"nie",囜:"nin",宁:"ning",妞:"niu",农:"nong",羺:"nou",奴:"nu",女:"nv",奻:"nuan",疟:"nüe",黁:"nun",挪:"nuo",喔:"o",讴:"ou",妑:"pa",拍:"pai",眅:"pan",乓:"pang",抛:"pao",呸:"pei",喷:"pen",匉:"peng",丕:"pi",囨:"pian",剽:"piao",氕:"pie",姘:"pin",乒:"ping",钋:"po",剖:"pou",仆:"pu",七:"qi",掐:"qia",千:"qian",呛:"qiang",悄:"qiao",苆:"qie",亲:"qin",靑:"qing",卭:"qiong",丘:"qiu",区:"qu",奍:"quan",缺:"que",夋:"qun",呥:"ran",穣:"rang",娆:"rao",惹:"re",人:"ren",扔:"reng",日:"ri",茸:"rong",厹:"rou",邚:"ru",挼:"rua",堧:"ruan",婑:"rui",瞤:"run",捼:"ruo",仨:"sa",毢:"sai",三:"san",桒:"sang",掻:"sao",閪:"se",森:"sen",僧:"seng",杀:"sha",筛:"shai",山:"shan",伤:"shang",弰:"shao",奢:"she",申:"shen",升:"sheng",尸:"shi",収:"shou",书:"shu",刷:"shua",衰:"shuai",闩:"shuan",双:"shuang",脽:"shui",吮:"shun",说:"shuo",厶:"si",忪:"song",凁:"sou",苏:"su",狻:"suan",夊:"sui",孙:"sun",唆:"suo",他:"ta",囼:"tai",坍:"tan",汤:"tang",夲:"tao",忑:"te",熥:"teng",剔:"ti",天:"tian",旫:"tiao",帖:"tie",厅:"ting",囲:"tong",偷:"tou",凸:"tu",湍:"tuan",推:"tui",吞:"tun",乇:"tuo",屲:"wa",歪:"wai",弯:"wan",尣:"wang",危:"wei",昷:"wen",翁:"weng",挝:"wo",乌:"wu",夕:"xi",虲:"xia",仙:"xian",乡:"xiang",灱:"xiao",些:"xie",心:"xin",星:"xing",凶:"xiong",休:"xiu",吁:"xu",吅:"xuan",削:"xue",坃:"xun",丫:"ya",恹:"yan",央:"yang",幺:"yao",倻:"ye",一:"yi",囙:"yin",应:"ying",哟:"yo",佣:"yong",优:"you",込:"yu",囦:"yuan",曰:"yue",晕:"yun",帀:"za",災:"zai",兂:"zan",匨:"zang",傮:"zao",则:"ze",贼:"zei",怎:"zen",増:"zeng",扎:"zha",夈:"zhai",枬:"zhan",张:"zhang",佋:"zhao",蜇:"zhe",贞:"zhen",凧:"zheng",之:"zhi",中:"zhong",州:"zhou",朱:"zhu",抓:"zhua",拽:"zhuai",专:"zhuan",妆:"zhuang",隹:"zhui",宒:"zhun",卓:"zhuo",乲:"zi",宗:"zong",邹:"zou",租:"zu",钻:"zuan",厜:"zui",尊:"zun",昨:"zuo"},r={沈:"shen",嗲:"dia",碡:"zhou",聒:"guo",炔:"que",蚵:"ke",砉:"hua",嬤:"mo",蹒:"pan",丬:"pan",霰:"xian",豉:"chi",饧:"xing",帧:"zhen",郍:"na",芎:"xiong",谁:"shui"},o=Object.keys(n),u=Object.values(n),c=Object.assign({},n,r);if(!!navigator.userAgent.match(/Version\/[\d.]+.*Safari/)){const s={丷:"八",抽:"婤",凑:"腠",厾:"艔",夫:"伕",勾:"佝",乎:"乯",欢:"犿",哩:"刕",毟:"列",伶:"刢",嚒:"么",椚:"门",擝:"甿",娘:"嬢",剖:"娝",苆:"癿",卭:"匔",奍:"峑",凁:"捜",偷:"偸",屲:"穵",仙:"仚",込:"扜",优:"攸",夈:"捚",枬:"沾",凧:"争",州:"诌"};Object.keys(s).forEach(function(p){o[o.indexOf(p)]=s[p]}),Object.assign(c,{凒:"ai",貋:"an",飹:"bao",蛽:"bei",榌:"bi",畁:"bi",禆:"bi",獱:"bian",溊:"bo",淿:"bo",鸔:"bu",廍:"bu",礸:"ca",荝:"ce",犲:"chai",仩:"chang",罉:"cheng",鐣:"cheng",粚:"chi",鵄:"chi",攡:"chi",肔:"chi",槆:"chun",媨:"cu",膥:"cun",迏:"da",迚:"da",跶:"da",亣:"da",侢:"dai",蚮:"dai",艔:"dao",悳:"de",枤:"di",刟:"diao",鈟:"diao",譵:"dui",遻:"e",笩:"fa",匥:"fan",仮:"fan",胐:"fei",襥:"fu",焹:"gang",稁:"gao",峼:"gao",搄:"gen",匔:"gong",簼:"gou",躀:"guan",褁:"guo",駴:"hai",呵:"he",寉:"he",燺:"he",玜:"hong",闀:"hong",蘤:"hua",諙:"hua",鯶:"huan",屶:"hui",檅:"hui",叝:"ji",躤:"ji",笅:"jiao",鞂:"jie",掶:"jie",媫:"jie",嶻:"jie",擮:"jie",礍:"jie",矝:"jin",煡:"jin",璄:"jing",燛:"jiong",鼰:"ju",烥:"ju",灍:"jue",鵕:"jun",钶:"ke",狅:"kuang",軠:"kuang",鋛:"kuang",嗠:"lao",矋:"lei",竰:"li",竂:"liao",謢:"lu",簵:"lu",率:"lv",绿:"lv",嘸:"m",袮:"mi",粎:"mi",杣:"mian",嫹:"miao",琝:"min",吶:"na",呐:"na",抩:"nan",煵:"nan",踙:"nie",倿:"ning",挵:"nong",衂:"nv",渒:"pai",萠:"pan",蘕:"peng",錃:"pi",蠯:"pi",酦:"po",炇:"pu",圑:"pu",暜:"pu",捿:"qi",紪:"qi",嵜:"qi",褀:"qi",騹:"qi",峠:"qia",偂:"qian",嬱:"qian",鞩:"qiao",厒:"qie",鈫:"qin",儬:"qing",濪:"qing",峵:"rong",钑:"sa",炶:"shan",椫:"shan",罙:"shen",鯓:"shen",枡:"sheng",鍟:"sheng",縄:"sheng",膄:"shou",毺:"shu",暺:"tan",砙:"wa",晥:"wan",瞣:"wan",榲:"wen",饂:"wen",攚:"weng",邜:"xi",湺:"xian",鷍:"xiao",炨:"xie",匂:"xiong",潃:"xiu",燅:"xun",訮:"yan",滧:"yao",摿:"yao",磘:"yao",洂:"ye",捙:"ye",璍:"ye",渏:"yi",頥:"yi",鈘:"yi",貖:"yi",袬:"yu",鴧:"yu",伝:"yun",齫:"yun",沯:"za",溨:"zai",煰:"zao",歵:"ze",茋:"zhi",隲:"zhi",钃:"zhu",赼:"zi",荢:"zi",燪:"zong",唨:"zu"})}const d=19968,q=40869,f=(s,x)=>{const p=s.charCodeAt(0);if(p<d||p>q)return s;if(c[s])return x==="first_letter"?c[s].charAt(0):c[s];let v=0,_=o.length-1,m=-1,H,R;for(;v<=_;)if(m=Math.floor((v+_)/2),H=o[m],R=s.localeCompare(H,["zh-CN"]),R===1)v=m+1;else if(R===-1)_=m-1;else break;return R<0&&m--,c[s]=u[m],x==="first_letter"?u[m].charAt(0):u[m]},i=(s,x)=>{if(typeof s!="string")return s;let p="";return _e(s,v=>{p+=f(v,x)}),p};return((s,x)=>(j||(j=i),j(s,x)))(e,a)};function Ae(e,a){let n=e;Re(n)||(n=""),n=n.toLowerCase();const r=W($(n),u=>u[0]).join(""),o=W($(n,"first_letter"),u=>u[0]).join("");return n.indexOf(a)>-1||r.indexOf(a)>-1||o.indexOf(a)>-1}const z={fieldProps:{showSearch:!0,filterOption:(e,a)=>Ae(a.label,e),fetchDataOnSearch:!1}},y={广州:["白云","黄埔","增城"],深圳:["福田","南山","宝安"]},A={HIGH:{text:"高"},MEDIUM:{text:"中"},LOW:{text:"低"}},fe=[{value:"0",label:"第一小学"},{value:"1",label:"第二小学"},{value:"2",label:"第三小学"}];function k(e){return Math.floor(Math.random()*e)}function Ne(){return Object.keys(y)[k(Object.keys(y).length+1)]}function Me(e){if(e.city)return e.city&&y[e.city][k(y[e.city].length+1)]}function Ve(){return Object.keys(A)[k(Object.keys(A).length+1)]}function Ee(e){return ze(e).map(a=>{const n=Ne(),r=Me({city:n});return{id:`${a}`,name:`这是名字这是名字这是名字 ${a}`,city:n,area:r,level:Ve(),status:k(2)===1,school:""+k(fe.length)}})}let h=Ee(21);async function P(e){return console.log("fakeRequest",e),new Promise(a=>{setTimeout(()=>{a({data:h,success:!0,total:h.length})},1e3)})}async function xe(e){return console.log("fakeDeleteByRecord",e),h=h.filter(a=>a.id!==e.id),Promise.resolve({})}async function V(e){return console.log("fakeGetByRecord",e),new Promise(a=>{setTimeout(()=>{a(h.find(n=>n.id===e.id))},1e3)})}async function E(e){return console.log("fakeCreate",e),h.push({id:h.length+1,...e}),Promise.resolve({})}async function G(e){return console.log("fakeUpdateById",e),h=h.map(a=>a.id===e.id?{...a,...e}:a),Promise.resolve({})}function Ge(){return Promise.resolve(Object.keys(y))}function He(e){return e!=null&&e.city?Promise.resolve(y[e.city]):Promise.resolve(Object.values(y).reduce((a,n)=>a.concat(n),[]))}function We(){return Promise.resolve(fe)}const sn={title:"@fe-free/core/CRUD",component:g,tags:["autodocs"]},I={render:()=>{const e=[{title:"id",dataIndex:"id",search:!0},{title:"名字(省略)",dataIndex:"name",search:!0,ellipsis:!0},{title:"city",dataIndex:"city"},{title:"area",dataIndex:"area"}];return t.jsx(g,{actions:["create","read","delete","update"],tableProps:{columns:e,request:P},requestDeleteByRecord:xe,deleteProps:{nameIndex:"name"},detailForm:a=>t.jsx(t.Fragment,{children:t.jsx(b,{...a,name:"name",label:"名字",required:!0,rules:[{required:!0}],extra:"extra extra extra extra"})}),requestGetByRecord:V,requestCreateByValues:E,requestUpdateById:G})}},F={render:()=>{const e=[{title:"id",dataIndex:"id",search:!0},{title:"名字",dataIndex:"name",search:!0}];return t.jsx(g,{actions:["read_detail"],tableProps:{columns:e,request:P}})}},$e=()=>{const e=M.useRef(),[a]=Ie.useForm(),n=[{title:"id",dataIndex:"id",search:!0},{title:"名字",dataIndex:"name",search:!0}];return t.jsx(g,{actions:["create","read","update"],tableProps:{formRef:e,columns:n,request:P},detailFormInstance:a,detailForm:r=>t.jsxs(t.Fragment,{children:[t.jsx(b,{...r,name:"name",label:"名字",required:!0,rules:[{required:!0}],initialValue:"default"}),t.jsx(Se,{...r,name:"status",label:"开启",initialValue:!1})]}),requestGetByRecord:V,requestCreateByValues:E,requestUpdateByValues:G})},B={render:()=>t.jsx($e,{})},Le=()=>{const e=M.useRef(),a=[{title:"id",dataIndex:"id",search:!0},{title:"名字",dataIndex:"name",search:!0}];return t.jsxs(t.Fragment,{children:[t.jsx(ge,{onClick:()=>{var n;return(n=e.current.getActionRef().current)==null?void 0:n.reload()},children:"reload"}),t.jsx(g,{ref:e,actions:[],tableProps:{columns:a,request:P}})]})},S={render:()=>t.jsx(Le,{})},w={render:()=>{const e=[{title:"id",dataIndex:"id",search:!0},{title:"名字",dataIndex:"name",search:!0},{title:"等级(本地数据)",dataIndex:"level",search:!0,valueEnum:A,...z},{title:"city(远端数据)",dataIndex:"city",search:!0,request:async()=>(await Ge()).map(n=>({label:n,value:n})),...z},{title:"area(联动 city)",dataIndex:"area",search:!0,request:async a=>(await He(a)).map(r=>({label:r,value:r})),dependencies:["city"],...z},{title:"学校(远端数据 label value)",dataIndex:"school",search:!0,valueType:"select",request:()=>We(),...z}];return t.jsx(g,{actions:[],tableProps:{columns:e,request:P}})}},T={render:()=>{const e=[{title:"id",dataIndex:"id"},{title:"名字",dataIndex:"name"}];return t.jsx(g,{actions:[],tableProps:{columns:e,request:P,search:!1}})}},O={render:()=>{const e=[{title:"id",dataIndex:"id",search:!0},{title:"名字",dataIndex:"name",search:!0}];return t.jsx(g,{actions:["create","read","delete","update"],tableProps:{columns:e,request:P},createButton:t.jsx(ge,{type:"primary",children:"新建"}),readProps:{operateText:"查看"},deleteProps:{nameIndex:"name",operateText:"删除",desc:"确定要删除吗？"},updateProps:{operateText:"编辑",successText:"编辑成功"},requestDeleteByRecord:xe,detailForm:a=>t.jsx(t.Fragment,{children:t.jsx(b,{...a,name:"name",label:"名字",required:!0,rules:[{required:!0}]})}),requestGetByRecord:V,requestCreateByValues:E,requestUpdateByValues:G})}};var L,Y,Z;I.parameters={...I.parameters,docs:{...(L=I.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: () => {
+    const columns = [{
+      title: 'id',
+      dataIndex: 'id',
+      search: true
+    }, {
+      title: '名字(省略)',
+      dataIndex: 'name',
+      search: true,
+      ellipsis: true
+    }, {
+      title: 'city',
+      dataIndex: 'city'
+    }, {
+      title: 'area',
+      dataIndex: 'area'
+    }];
+    return <CRUD actions={['create', 'read', 'delete', 'update']} tableProps={{
+      columns,
+      request: fakeRequest
+    }} requestDeleteByRecord={fakeDeleteByRecord} deleteProps={{
+      nameIndex: 'name'
+    }} detailForm={formProps => <>
+            <ProFormText {...formProps} name="name" label="名字" required rules={[{
+        required: true
+      }]} extra="extra extra extra extra" />
+          </>} requestGetByRecord={fakeGetByRecord} requestCreateByValues={fakeCreate} requestUpdateById={fakeUpdateById} />;
+  }
+}`,...(Z=(Y=I.parameters)==null?void 0:Y.docs)==null?void 0:Z.source}}};var J,K,Q;F.parameters={...F.parameters,docs:{...(J=F.parameters)==null?void 0:J.docs,source:{originalSource:`{
+  render: () => {
+    const columns = [{
+      title: 'id',
+      dataIndex: 'id',
+      search: true
+    }, {
+      title: '名字',
+      dataIndex: 'name',
+      search: true
+    }];
+    return <CRUD actions={['read_detail']} tableProps={{
+      columns,
+      request: fakeRequest
+    }} />;
+  }
+}`,...(Q=(K=F.parameters)==null?void 0:K.docs)==null?void 0:Q.source}}};var X,ee,ne;B.parameters={...B.parameters,docs:{...(X=B.parameters)==null?void 0:X.docs,source:{originalSource:`{
+  render: () => <RefComponent />
+}`,...(ne=(ee=B.parameters)==null?void 0:ee.docs)==null?void 0:ne.source}}};var ae,te,re;S.parameters={...S.parameters,docs:{...(ae=S.parameters)==null?void 0:ae.docs,source:{originalSource:`{
+  render: () => <ActionRefComponent />
+}`,...(re=(te=S.parameters)==null?void 0:te.docs)==null?void 0:re.source}}};var oe,ie,se;w.parameters={...w.parameters,docs:{...(oe=w.parameters)==null?void 0:oe.docs,source:{originalSource:`{
+  render: () => {
+    const columns: ProColumns<any>[] = [{
+      title: 'id',
+      dataIndex: 'id',
+      search: true
+    }, {
+      title: '名字',
+      dataIndex: 'name',
+      search: true
+    }, {
+      title: '等级(本地数据)',
+      dataIndex: 'level',
+      search: true,
+      valueEnum: levels,
+      ...proFormSelectSearchProps
+    }, {
+      title: 'city(远端数据)',
+      dataIndex: 'city',
+      search: true,
+      request: async () => {
+        const res = await fakeRequestCity();
+        return res.map(item => ({
+          label: item,
+          value: item
+        }));
+      },
+      ...proFormSelectSearchProps
+    }, {
+      title: 'area(联动 city)',
+      dataIndex: 'area',
+      search: true,
+      request: async params => {
+        const res = await fakeRequestArea(params);
+        return res.map(item => ({
+          label: item,
+          value: item
+        }));
+      },
+      dependencies: ['city'],
+      ...proFormSelectSearchProps
+    }, {
+      title: '学校(远端数据 label value)',
+      dataIndex: 'school',
+      search: true,
+      valueType: 'select' as const,
+      request: () => fakeRequestSchool(),
+      ...proFormSelectSearchProps
+    }];
+    return <CRUD actions={[]} tableProps={{
+      columns,
+      request: fakeRequest
+    }} />;
+  }
+}`,...(se=(ie=w.parameters)==null?void 0:ie.docs)==null?void 0:se.source}}};var ue,ce,de;T.parameters={...T.parameters,docs:{...(ue=T.parameters)==null?void 0:ue.docs,source:{originalSource:`{
+  render: () => {
+    const columns = [{
+      title: 'id',
+      dataIndex: 'id'
+    }, {
+      title: '名字',
+      dataIndex: 'name'
+    }];
+    return <CRUD actions={[]} tableProps={{
+      columns,
+      request: fakeRequest,
+      search: false
+    }} />;
+  }
+}`,...(de=(ce=T.parameters)==null?void 0:ce.docs)==null?void 0:de.source}}};var le,he,pe;O.parameters={...O.parameters,docs:{...(le=O.parameters)==null?void 0:le.docs,source:{originalSource:`{
+  render: () => {
+    const columns = [{
+      title: 'id',
+      dataIndex: 'id',
+      search: true
+    }, {
+      title: '名字',
+      dataIndex: 'name',
+      search: true
+    }];
+    return <CRUD actions={['create', 'read', 'delete', 'update']} tableProps={{
+      columns,
+      request: fakeRequest
+    }} createButton={<Button type="primary">新建</Button>} readProps={{
+      operateText: '查看'
+    }} deleteProps={{
+      nameIndex: 'name',
+      operateText: '删除',
+      desc: '确定要删除吗？'
+    }} updateProps={{
+      operateText: '编辑',
+      successText: '编辑成功'
+    }} requestDeleteByRecord={fakeDeleteByRecord} detailForm={formProps => <>
+            <ProFormText {...formProps} name="name" label="名字" required rules={[{
+        required: true
+      }]} />
+          </>} requestGetByRecord={fakeGetByRecord} requestCreateByValues={fakeCreate} requestUpdateByValues={fakeUpdateById} />;
+  }
+}`,...(pe=(he=O.parameters)==null?void 0:he.docs)==null?void 0:pe.source}}};const un=["Normal","ReadDetail","Ref","ActionRef","RemoteData","NoSearch","CustomText"];export{S as ActionRef,O as CustomText,T as NoSearch,I as Normal,F as ReadDetail,B as Ref,w as RemoteData,un as __namedExportsOrder,sn as default};
