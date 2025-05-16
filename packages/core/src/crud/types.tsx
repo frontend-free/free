85ce2682cd97da7742cd83d996eb715c8ed9671f
liyatang <liyatang@qq.com>
2025-05-16 11:00:30 +0800

    feat: some

--- packages/core/src/crud/types.tsx
+++ packages/core/src/crud/types.tsx
@@ -14,11 +14,30 @@ type CrudAction = 'create' | 'read' | 'read_detail' | 'update' | 'delete';
 interface CRUDProps<DataSource = any, Key = string> {
   actions: CrudAction[];
 
+  // *** 表单 ***
+
+  /** 弹窗表单 */
+  detailForm?: (formProps: { readonly: boolean }, info: { action: CrudAction }) => ReactNode;
+  /** detailForm 的 formRef */
+  detailFormInstance?: ProFormInstance;
+
+  // *** Create 新建 ***
+
   /** 新建按钮，默认新建 */
   createButton?: ReactNode;
+  /** 新增接口 */
+  requestCreateByValues?: (values: Partial<DataSource>) => Promise<any>;
+  /** create 更多设置 */
+  createProps?: {
+    /** 成功文案 */
+    successText?: string | (() => string);
+  };
+
+  // *** Read 表格 ***
 
   /** 表格相关 */
   tableProps: TableProps<DataSource>;
+  /** 表格操作列相关 */
   operateColumnProps?: {
     width?: number;
     /** 扩展操作区域，再其他操作之前 */
@@ -26,12 +45,35 @@ interface CRUDProps<DataSource = any, Key = string> {
     /** 扩展操作区域，在其他操作之后 */
     moreOperatorAfter?: (record: DataSource) => ReactNode;
   };
+
+  // *** Read 详情 ***
+
+  /** 获取详情接口 */
+  requestGetByRecord?: (record: DataSource) => Promise<any>;
+  /** 跳转到详情的索引 ，默认 id */
+  detailIdIndex?: string;
+  /** read 更多设置 */
   readProps?: {
     /** 文本 */
     operateText?: string;
     /** 打开方式, action 为 read_detail 有效 */
     target?: '_blank';
   };
+
+  // *** Update 更新 ***
+
+  /** 更新接口 */
+  requestUpdateByValues?: (values: Partial<DataSource>) => Promise<any>;
+  /** @deprecated 请使用 requestUpdateByValues 替代 */
+  requestUpdateById?: (values: Partial<DataSource>) => Promise<any>;
+  updateProps?: {
+    /** 文本 */
+    operateText?: string;
+    /** 成功文案 */
+    successText?: string | (() => string);
+  };
+
+  // *** Delete 删除 ***
 
   /** 删除接口 */
   requestDeleteByRecord?: (record: DataSource) => Promise<any>;
@@ -45,34 +87,7 @@ interface CRUDProps<DataSource = any, Key = string> {
     operateText?: string;
   };
 
-  /** 弹窗表单 */
-  detailForm?: (formProps: { readonly: boolean }, info: { action: CrudAction }) => ReactNode;
-  /** detailForm 的 formRef */
-  detailFormInstance?: ProFormInstance;
-
-  /** 新增接口 */
-  requestCreateByValues?: (values: Partial<DataSource>) => Promise<any>;
-  createProps?: {
-    /** 成功文案 */
-    successText?: string | (() => string);
-  };
-
-  /** 更新接口 */
-  requestUpdateByValues?: (values: Partial<DataSource>) => Promise<any>;
-  /** @deprecated 请使用 requestUpdateByValues 替代 */
-  requestUpdateById?: (values: Partial<DataSource>) => Promise<any>;
-  updateProps?: {
-    /** 文本 */
-    operateText?: string;
-    /** 成功文案 */
-    successText?: string | (() => string);
-  };
-
-  /** 获取详情接口 */
-  requestGetByRecord?: (record: DataSource) => Promise<any>;
-
-  /** 跳转到详情的索引 ，默认 id */
-  detailIdIndex?: string;
+  // *** 批量操作 ***
 
   /** 批量操作 */
   batchActions?: {
