423bea66ba08387895a531c81449782d30c0c205
liyatang <liyatang@users.noreply.github.com>
2025-05-16 02:48:16 +0000

    deploy: 503c26b41aa52c0f7241e1e0548a1135fa994252

---
+++ assets/editor_markdown.stories-Bcz9huQO.js
@@ -0,0 +1,6 @@
+import{j as o}from"./jsx-runtime-BjgbQsUx.js";import{c as a}from"./index-BkE265hr.js";import"./date-CuSWkN_9.js";import{r as m}from"./index-BTkrZ5ys.js";import"./index-D2MAbzvX.js";import"./index-BqOStK5P.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BIm0odtm.js";import"./iframe-BxdolZCe.js";import"../sb-preview/runtime.js";const w={title:"@fe-free/core/EditorMarkdown",component:a,tags:["autodocs"]},c=()=>{const[i]=m.useState(`# hello
+
+world
+`);return o.jsx("div",{style:{width:"500px",height:"500px"},children:o.jsx(a,{value:i})})},r={render:()=>o.jsx(c,{})};var t,e,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => <BasicDemo />
+}`,...(s=(e=r.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const B=["Basic"];export{r as Basic,B as __namedExportsOrder,w as default};
