3b0a6fe20c4ea4fd464680e33d5860cc2615c597
liyatang <liyatang@users.noreply.github.com>
2025-05-23 08:35:45 +0000

    deploy: 326158e401c5cd5589574732a3301400f6903759

---
+++ assets/button.stories-K2ryo7tl.js
@@ -0,0 +1,19 @@
+import{L as l}from"./index-Cp8hvRtj.js";import"./jsx-runtime-BjgbQsUx.js";import"./date-CuSWkN_9.js";import"./index-BTkrZ5ys.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D2MAbzvX.js";import"./index-BqOStK5P.js";import"./index-BIm0odtm.js";import"./iframe-g1IBkNKD.js";import"../sb-preview/runtime.js";const C={title:"@fe-free/core/LoadingButton",component:l,tags:["autodocs"]},e={args:{children:"click and resolve",onClick:()=>new Promise(o=>{setTimeout(o,1e3)})}},r={args:{children:"click and reject",onClick:()=>new Promise((o,m)=>{setTimeout(m,1e3)})}};var t,n,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    children: 'click and resolve',
+    onClick: () => {
+      return new Promise(resolve => {
+        setTimeout(resolve, 1000);
+      });
+    }
+  }
+}`,...(s=(n=e.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};var c,i,a;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: 'click and reject',
+    onClick: () => {
+      return new Promise((_, reject) => {
+        setTimeout(reject, 1000);
+      });
+    }
+  }
+}`,...(a=(i=r.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};const P=["Resolve","Reject"];export{r as Reject,e as Resolve,P as __namedExportsOrder,C as default};
---
+++ assets/editor_markdown.stories-DasHK-Ah.js
@@ -0,0 +1,6 @@
+import{j as o}from"./jsx-runtime-BjgbQsUx.js";import{f as a}from"./index-Cp8hvRtj.js";import"./date-CuSWkN_9.js";import{r as m}from"./index-BTkrZ5ys.js";import"./index-D2MAbzvX.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BqOStK5P.js";import"./index-BIm0odtm.js";import"./iframe-g1IBkNKD.js";import"../sb-preview/runtime.js";const w={title:"@fe-free/core/EditorMarkdown",component:a,tags:["autodocs"]},p=()=>{const[i]=m.useState(`# hello
+
+world
+`);return o.jsx("div",{style:{width:"500px",height:"500px"},children:o.jsx(a,{value:i})})},r={render:()=>o.jsx(p,{})};var t,e,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => <BasicDemo />
+}`,...(s=(e=r.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const B=["Basic"];export{r as Basic,B as __namedExportsOrder,w as default};
