b8ceaa5ccd338f28c4406150b42411c5df4066ee
liyatang <liyatang@users.noreply.github.com>
2025-05-26 10:20:37 +0000

    deploy: c3a9c5a667a0eb37e71e0985ab069100c91e4fe0

---
+++ assets/crud_of_simple.stories-Cur9Yo45.js
@@ -0,0 +1,51 @@
+import{j as e}from"./jsx-runtime-BjgbQsUx.js";import{C as o}from"./index-BYvFW7kM.js";import"./isNumber-Drx5NVlI.js";import"./date-DYq0-6zX.js";import{f as c,a as p,b as x}from"./data-BC-jWGps.js";import{W as f}from"./index-CEI_Hkl8.js";import"./index-D2MAbzvX.js";import"./index-BTkrZ5ys.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BqOStK5P.js";import"./index-BIm0odtm.js";import"./iframe-BgOMTvdf.js";import"../sb-preview/runtime.js";const F={title:"@fe-free/core/CRUDOfSimple",component:o,tags:["autodocs"]},r={render:()=>{const a=[{title:"id",dataIndex:"id",search:!0},{title:"名字(省略)",dataIndex:"name",search:!0,ellipsis:!0}];return e.jsx(o,{actions:["create","delete"],tableProps:{columns:a,request:c,pagination:!1},requestDeleteByRecord:p,deleteProps:{nameIndex:"name"},detailForm:s=>e.jsx(e.Fragment,{children:e.jsx(f,{...s,name:"name",label:"名字",required:!0,rules:[{required:!0}],extra:"extra extra extra extra"})}),requestCreateByValues:x})}},t={render:()=>{const a=[{title:"id",dataIndex:"id",search:!0},{title:"名字(省略)",dataIndex:"name",search:!0,ellipsis:!0}];return e.jsx(o,{actions:["create","delete"],tableProps:{columns:a,request:c,pagination:!1},requestDeleteByRecord:p,deleteProps:{nameIndex:"name"},detailForm:s=>e.jsx(e.Fragment,{children:e.jsx(f,{...s,name:"name",label:"名字",required:!0,rules:[{required:!0}],extra:"extra extra extra extra"})}),requestCreateByValues:x,simpleSearchProps:{name:"id"}})}};var i,l,m;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: () => {
+    const columns = [{
+      title: 'id',
+      dataIndex: 'id',
+      search: true
+    }, {
+      title: '名字(省略)',
+      dataIndex: 'name',
+      search: true,
+      ellipsis: true
+    }];
+    return <CRUDOfSimple actions={['create', 'delete']} tableProps={{
+      columns,
+      request: fakeRequest,
+      pagination: false
+    }} requestDeleteByRecord={fakeDeleteByRecord} deleteProps={{
+      nameIndex: 'name'
+    }} detailForm={formProps => <>
+            <ProFormText {...formProps} name="name" label="名字" required rules={[{
+        required: true
+      }]} extra="extra extra extra extra" />
+          </>} requestCreateByValues={fakeCreate} />;
+  }
+}`,...(m=(l=r.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};var n,d,u;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: () => {
+    const columns = [{
+      title: 'id',
+      dataIndex: 'id',
+      search: true
+    }, {
+      title: '名字(省略)',
+      dataIndex: 'name',
+      search: true,
+      ellipsis: true
+    }];
+    return <CRUDOfSimple actions={['create', 'delete']} tableProps={{
+      columns,
+      request: fakeRequest,
+      pagination: false
+    }} requestDeleteByRecord={fakeDeleteByRecord} deleteProps={{
+      nameIndex: 'name'
+    }} detailForm={formProps => <>
+            <ProFormText {...formProps} name="name" label="名字" required rules={[{
+        required: true
+      }]} extra="extra extra extra extra" />
+          </>} requestCreateByValues={fakeCreate} simpleSearchProps={{
+      name: 'id'
+    }} />;
+  }
+}`,...(u=(d=t.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};const j=["Normal","WithSearch"];export{r as Normal,t as WithSearch,j as __namedExportsOrder,F as default};
--- assets/editor_markdown.stories-BigGBFF2.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./jsx-runtime-BjgbQsUx.js";import{g as a}from"./index-DTEILdXT.js";import"./isNumber-Drx5NVlI.js";import"./date-DYq0-6zX.js";import{r as m}from"./index-BTkrZ5ys.js";import"./index-D2MAbzvX.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BqOStK5P.js";import"./index-BIm0odtm.js";import"./iframe-C52CROUC.js";import"../sb-preview/runtime.js";const w={title:"@fe-free/core/EditorMarkdown",component:a,tags:["autodocs"]},p=()=>{const[i]=m.useState(`# hello
-
-world
-`);return o.jsx("div",{style:{width:"500px",height:"500px"},children:o.jsx(a,{value:i})})},r={render:()=>o.jsx(p,{})};var t,e,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: () => <BasicDemo />
-}`,...(s=(e=r.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const B=["Basic"];export{r as Basic,B as __namedExportsOrder,w as default};
