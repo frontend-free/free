63f6a7cba14242137f5d4e791b3885006211cd41
liyatang <liyatang@users.noreply.github.com>
2025-05-27 12:28:41 +0000

    deploy: 1fe7fe9a422dc53d90f827f075ef6d9c131834f0

--- assets/editor_markdown.stories-DWlGW6je.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./jsx-runtime-BjgbQsUx.js";import{g as a}from"./pro_form_switch_number-D7z2PTTV.js";import"./date-BDUOmFEF.js";import"./isNumber-Dc4sICNo.js";import{r as m}from"./index-BTkrZ5ys.js";import"./index-D2MAbzvX.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BqOStK5P.js";import"./index-BIm0odtm.js";import"./iframe-COJEM47A.js";import"../sb-preview/runtime.js";const w={title:"@fe-free/core/EditorMarkdown",component:a,tags:["autodocs"]},p=()=>{const[i]=m.useState(`# hello
-
-world
-`);return o.jsx("div",{style:{width:"500px",height:"500px"},children:o.jsx(a,{value:i})})},r={render:()=>o.jsx(p,{})};var t,e,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: () => <BasicDemo />
-}`,...(s=(e=r.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const B=["Basic"];export{r as Basic,B as __namedExportsOrder,w as default};
