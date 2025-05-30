fe27cf4206fe9ff53ec00e06a93c314dd711ebb2
liyatang <liyatang@users.noreply.github.com>
2025-05-30 06:48:46 +0000

    deploy: 8e962b520f39d5fca070ee7a27a4eb03ee627717

---
+++ assets/editor_logs.stories-NmfsvOap.js
@@ -0,0 +1,27 @@
+import{j as e}from"./jsx-runtime-BjgbQsUx.js";import{f as r}from"./pro_form_switch_number-BK2n5nEX.js";import"./date-DJ5b9kEY.js";import"./isNumber-Dc4sICNo.js";import"./index-D2MAbzvX.js";import"./index-BTkrZ5ys.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BqOStK5P.js";import"./index-BIm0odtm.js";import"./iframe-e3eL5yDf.js";import"../sb-preview/runtime.js";const b={title:"@fe-free/core/EditorLogs",component:r,tags:["autodocs"],parameters:{docs:{description:{component:"一个用于显示日志内容的 React 组件。它使用 CodeMirror 编辑器来呈现日志，提供了语法高亮和主题支持。"}}}},s={args:{logs:[{timestamp:"2023-01-01 12:00:00",level:"info",message:"This is an info log message."},{timestamp:"2023-01-01 12:00:00",level:"warn",message:"This is a warning log message."},{timestamp:"2023-01-01 12:00:00",level:"error",message:"This is an error log message."},{timestamp:"2023-01-01 12:00:00",level:"system",message:"This is a debug log message. This is a debug log message This is a debug log message This is a debug log message This is a debug log message"}]},render:m=>e.jsx("div",{style:{width:"500px",height:"500px"},children:e.jsx(r,{...m})})};var i,a,o;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    logs: [{
+      timestamp: '2023-01-01 12:00:00',
+      level: 'info',
+      message: 'This is an info log message.'
+    }, {
+      timestamp: '2023-01-01 12:00:00',
+      level: 'warn',
+      message: 'This is a warning log message.'
+    }, {
+      timestamp: '2023-01-01 12:00:00',
+      level: 'error',
+      message: 'This is an error log message.'
+    }, {
+      timestamp: '2023-01-01 12:00:00',
+      level: 'system',
+      message: 'This is a debug log message. This is a debug log message This is a debug log message This is a debug log message This is a debug log message'
+    }]
+  },
+  render: props => <div style={{
+    width: '500px',
+    height: '500px'
+  }}>
+      <EditorLogs {...props} />
+    </div>
+}`,...(o=(a=s.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const f=["Basic"];export{s as Basic,f as __namedExportsOrder,b as default};
