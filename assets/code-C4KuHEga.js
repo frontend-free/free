78d3ab5cce8584cd0b5cddeb002088556aa23b37
liyatang <liyatang@users.noreply.github.com>
2025-06-06 07:58:24 +0000

    deploy: 7e7ac6a829d4a6a31622d5da3297a5a48d4ff8dc

---
+++ assets/code-C4KuHEga.js
@@ -0,0 +1,90 @@
+import{j as n}from"./jsx-runtime-BjgbQsUx.js";import{useMDXComponents as r}from"./index-BwvAoW5K.js";import{ae as i}from"./index-7ULWzJ3q.js";import"./index-D2MAbzvX.js";import"./index-BTkrZ5ys.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-CA1Oc5CG.js";import"../sb-preview/runtime.js";import"./index-6JRucVBG.js";import"./index-BIm0odtm.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";function o(t){const e={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{title:"@fe-free/free-scripts/build-code"}),`
+`,n.jsx(e.h1,{id:"build-code",children:"Build Code"}),`
+`,n.jsx(e.p,{children:"这是一个用于构建代码的工具。"}),`
+`,n.jsx(e.h2,{id:"build-code-1",children:"build-code"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-json",children:`// package.json
+{
+  "scripts": {
+    "build-enums": "free-scripts build-code -i ./enums.config.js -o ./src/code -t antd && prettier ./src/code --write"
+  }
+}
+`})}),`
+`,n.jsx(e.p,{children:"enums.config.js 配置如下"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-typescript",children:`const enums = {
+  SomeThing: {
+    // 建议大写开头
+    STRING: {
+      text: '文本',
+      value: 'string',
+    },
+    NUMBER: {
+      text: '数字',
+      // 如果不提供 value，则默认和 Key 一致
+      // value: 'NUMBER',
+    },
+    Time: {
+      text: '时间',
+      value: 'time',
+      // color 用于 Tag 的 color
+      color: 'green',
+      // status pro-components 的 valueEnum 定义 'Success' | 'Error' | 'Processing' | 'Warning' | 'Default';
+      status: 'Default',
+    },
+  },
+};
+module.exports = { enums };
+`})}),`
+`,n.jsx(e.p,{children:"会生成如下代码，供使用"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-typescript",children:`import type { TagProps } from 'antd';
+import { Tag } from 'antd';
+
+// --- SomeThing
+
+export enum EnumSomeThing {
+  STRING = 'string',
+  NUMBER = 'NUMBER',
+  Time = 'time',
+}
+
+export const valueEnumSomeThing = {
+  [EnumSomeThing.STRING]: {
+    text: '文本',
+    value: 'string',
+  },
+  [EnumSomeThing.NUMBER]: {
+    text: '数字',
+    value: 'NUMBER',
+  },
+  [EnumSomeThing.Time]: {
+    text: '时间',
+    value: 'time',
+    status: 'Default',
+    color: 'green',
+  },
+};
+
+export const listSomeThing = Object.keys(valueEnumSomeThing).map((key) => {
+  const item = valueEnumSomeThing[key];
+
+  return {
+    value: item.value !== undefined ? item.value : key,
+    label: item.text,
+    originData: item.data,
+  };
+});
+
+export function TagSomeThing(props: { value?: EnumSomeThing | string } & TagProps) {
+  const item = props.value && valueEnumSomeThing[props.value];
+
+  if (item) {
+    return (
+      <Tag color={item.color} {...props}>
+        {item.text}
+      </Tag>
+    );
+  }
+
+  return null;
+}
+
+`})})]})}function j(t={}){const{wrapper:e}={...r(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{j as default};
---
+++ assets/editor.stories-D3bNOLVb.js
@@ -0,0 +1,90 @@
+import{j as e}from"./jsx-runtime-BjgbQsUx.js";import{E as P}from"./pro_form_switch_number-BIoPRE9H.js";import"./date-DJ5b9kEY.js";import"./isNumber-Dc4sICNo.js";import{r as V}from"./index-BTkrZ5ys.js";import"./index-D2MAbzvX.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-6JRucVBG.js";import"./index-BIm0odtm.js";import"./iframe-CA1Oc5CG.js";import"../sb-preview/runtime.js";const rr={title:"@fe-free/core/Editor",component:P,tags:["autodocs"]},o=r=>{const[_,C]=V.useState(r.value||"");return e.jsx("div",{style:{width:"500px",height:"500px"},children:e.jsx(P,{...r,value:_,onChange:C})})},a={args:{value:"hello world!"},render:r=>e.jsx(o,{...r})},l={args:{value:"hello world!",readOnly:!0},render:r=>e.jsx(o,{...r})},s={args:{value:"hello world!hello world!hello world!hello world!hello world!hello world!hello world!hello world!hello world!hello world!hello world!hello world!hello world!hello world!hello world!",lineWrapping:!0},render:r=>e.jsx(o,{...r})},n={args:{value:"hello world!",editable:!1},render:r=>e.jsx(o,{...r})},t={args:{value:"",autoFocus:!0},render:r=>e.jsx(o,{...r})},c={args:{language:"javascript",value:'console.log("Hello, World!")'},render:r=>e.jsx(o,{...r})},d={args:{language:"python",value:`def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+# 测试斐波那契数列
+for i in range(10):
+    print(f"fibonacci({i}) = {fibonacci(i)}")`},render:r=>e.jsx(o,{...r})},i={args:{language:"json",value:'{"action": "hello", "data": "world"}'},render:r=>e.jsx(o,{...r})},p={args:{language:"markdown",value:`# Hello, World!
+
+This is a markdown file.
+
+## Title2
+
+### Title 3
+
+- Item 1
+- Item 2
+- Item 3`},render:r=>e.jsx(o,{...r})};var u,m,g;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    value: 'hello world!'
+  },
+  render: props => <BasicDemo {...props} />
+}`,...(g=(m=a.parameters)==null?void 0:m.docs)==null?void 0:g.source}}};var h,w,f;l.parameters={...l.parameters,docs:{...(h=l.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    value: 'hello world!',
+    readOnly: true
+  },
+  render: props => <BasicDemo {...props} />
+}`,...(f=(w=l.parameters)==null?void 0:w.docs)==null?void 0:f.source}}};var v,x,j;s.parameters={...s.parameters,docs:{...(v=s.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    value: 'hello world!hello world!hello world!hello world!hello world!hello world!hello world!hello world!hello world!hello world!hello world!hello world!hello world!hello world!hello world!',
+    lineWrapping: true
+  },
+  render: props => <BasicDemo {...props} />
+}`,...(j=(x=s.parameters)==null?void 0:x.docs)==null?void 0:j.source}}};var b,B,S;n.parameters={...n.parameters,docs:{...(b=n.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    value: 'hello world!',
+    editable: false
+  },
+  render: props => <BasicDemo {...props} />
+}`,...(S=(B=n.parameters)==null?void 0:B.docs)==null?void 0:S.source}}};var D,y,E;t.parameters={...t.parameters,docs:{...(D=t.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    value: '',
+    autoFocus: true
+  },
+  render: props => <BasicDemo {...props} />
+}`,...(E=(y=t.parameters)==null?void 0:y.docs)==null?void 0:E.source}}};var W,O,k;c.parameters={...c.parameters,docs:{...(W=c.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    language: 'javascript',
+    value: 'console.log("Hello, World!")'
+  },
+  render: props => <BasicDemo {...props} />
+}`,...(k=(O=c.parameters)==null?void 0:O.docs)==null?void 0:k.source}}};var I,T,F;d.parameters={...d.parameters,docs:{...(I=d.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    language: 'python',
+    value: \`def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+# 测试斐波那契数列
+for i in range(10):
+    print(f"fibonacci({i}) = {fibonacci(i)}")\`
+  },
+  render: props => <BasicDemo {...props} />
+}`,...(F=(T=d.parameters)==null?void 0:T.docs)==null?void 0:F.source}}};var H,J,R;i.parameters={...i.parameters,docs:{...(H=i.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    language: 'json',
+    value: '{"action": "hello", "data": "world"}'
+  },
+  render: props => <BasicDemo {...props} />
+}`,...(R=(J=i.parameters)==null?void 0:J.docs)==null?void 0:R.source}}};var A,M,N;p.parameters={...p.parameters,docs:{...(A=p.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    language: 'markdown',
+    value: \`# Hello, World!
+
+This is a markdown file.
+
+## Title2
+
+### Title 3
+
+- Item 1
+- Item 2
+- Item 3\`
+  },
+  render: props => <BasicDemo {...props} />
+}`,...(N=(M=p.parameters)==null?void 0:M.docs)==null?void 0:N.source}}};const er=["Basic","ReadOnly","Wrap","Editable","AutoFocus","Javascript","Python","JSON","Markdown"];export{t as AutoFocus,a as Basic,n as Editable,i as JSON,c as Javascript,p as Markdown,d as Python,l as ReadOnly,s as Wrap,er as __namedExportsOrder,rr as default};
