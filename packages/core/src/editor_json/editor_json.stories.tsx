99560c988db36dc4fec631ccc00e7a45a34c5676
liyatang <liyatang@qq.com>
2025-07-14 11:38:23 +0800

    feat: some

--- packages/core/src/editor_json/editor_json.stories.tsx
+++ packages/core/src/editor_json/editor_json.stories.tsx
@@ -11,94 +11,78 @@ const meta: Meta<typeof EditorJSON> = {
 export default meta;
 type Story = StoryObj<typeof EditorJSON>;
 
-// 基础示例
-const BasicDemo = () => {
-  const [value, setValue] = useState(
-    JSON.stringify(
-      {
-        name: 'world',
-      },
-      null,
-      2,
-    ),
-  );
-
-  return (
-    <div style={{ width: '500px', height: '500px' }}>
-      <EditorJSON value={value} onChange={setValue} />
-    </div>
-  );
-};
-
 export const Basic: Story = {
-  render: () => <BasicDemo />,
-};
-
-// 只读模式
-const ReadonlyDemo = () => {
-  const [value, setValue] = useState(
-    JSON.stringify(
-      {
+  render: (args) => {
+    const [value, setValue] = useState(
+      JSON.stringify({
         name: 'world',
-      },
-      null,
-      2,
-    ),
-  );
+      }),
+    );
 
-  return (
-    <div style={{ width: '500px', height: '500px' }}>
-      <EditorJSON value={value} onChange={setValue} readonly />
-    </div>
-  );
+    return (
+      <div style={{ width: '500px', height: '500px' }}>
+        <EditorJSON {...args} value={value} onChange={setValue} />
+      </div>
+    );
+  },
 };
 
 export const Readonly: Story = {
-  render: () => <ReadonlyDemo />,
-};
+  render: () => {
+    const [value, setValue] = useState(
+      JSON.stringify(
+        {
+          name: 'world',
+        },
+        null,
+        2,
+      ),
+    );
 
-// 树形模式
-const TreeModeDemo = () => {
-  const [value, setValue] = useState(
-    JSON.stringify(
-      {
-        name: 'world',
-      },
-      null,
-      2,
-    ),
-  );
-
-  return (
-    <div style={{ width: '500px', height: '500px' }}>
-      <EditorJSON value={value} onChange={setValue} mode="tree" readonly />
-    </div>
-  );
+    return (
+      <div style={{ width: '500px', height: '500px' }}>
+        <EditorJSON value={value} onChange={setValue} readonly />
+      </div>
+    );
+  },
 };
 
 export const TreeMode: Story = {
-  render: () => <TreeModeDemo />,
-};
+  render: () => {
+    const [value, setValue] = useState(
+      JSON.stringify(
+        {
+          name: 'world',
+        },
+        null,
+        2,
+      ),
+    );
 
-// 无菜单栏的只读树形模式
-const NoMenuBarDemo = () => {
-  const [value, setValue] = useState(
-    JSON.stringify(
-      {
-        name: 'world',
-      },
-      null,
-      2,
-    ),
-  );
-
-  return (
-    <div style={{ width: '500px', height: '500px' }}>
-      <EditorJSON value={value} onChange={setValue} readonly mode="tree" mainMenuBar={false} />
-    </div>
-  );
+    return (
+      <div style={{ width: '500px', height: '500px' }}>
+        <EditorJSON value={value} onChange={setValue} mode="tree" readonly />
+      </div>
+    );
+  },
 };
 
 export const NoMenuBar: Story = {
-  render: () => <NoMenuBarDemo />,
+  render: () => {
+    const [value, setValue] = useState(
+      JSON.stringify(
+        {
+          name: 'world',
+        },
+        null,
+        2,
+      ),
+    );
+
+    return (
+      <div style={{ width: '500px', height: '500px' }}>
+        <EditorJSON value={value} onChange={setValue} readonly mode="tree" mainMenuBar={false} />
+      </div>
+    );
+  },
 };
