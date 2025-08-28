f37ca824b689a13bf9c54109ce6e2a999aedf0b1
liyatang <liyatang@users.noreply.github.com>
2025-08-28 08:02:52 +0000

    deploy: c756bbd6cfeae75524c43600e22eb1cfa1190743

---
+++ assets/copy.stories-DsmjxtAv.js
@@ -0,0 +1,27 @@
+import{C as a}from"./localforage-BzOjR7yL.js";import"./jsx-runtime-CWxYxdN8.js";import"./date-CBlPaY01.js";import"./_commonjs-dynamic-modules-CVDP7VWI.js";import"./iframe-BwT8Hu9S.js";import"./isNumber-D2s6sUQp.js";import"./index-r6yxhA4T.js";import"./index-noVmSWaV.js";import"./preload-helper-D9Z9MdNV.js";const h={title:"@fe-free/core/Copy",component:a,tags:["autodocs"],parameters:{docs:{description:{component:"基于 antd 的 Copy 组件，自动根据 value 显示复制按钮"}}}},r={args:{value:"点击复制",children:"children 点击复制"}},e={args:{value:"icon复制",showIcon:!0,children:"children 点击复制"}},o={args:{value:"hover复制",showIcon:!0,hoverIcon:!0,children:"children 点击复制"}},n={args:{value:"点击复制",onCopied:()=>{alert("复制成功")},children:"children 点击复制"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: '点击复制',
+    children: 'children 点击复制'
+  }
+}`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: 'icon复制',
+    showIcon: true,
+    children: 'children 点击复制'
+  }
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: 'hover复制',
+    showIcon: true,
+    hoverIcon: true,
+    children: 'children 点击复制'
+  }
+}`,...o.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: '点击复制',
+    onCopied: () => {
+      alert('复制成功');
+    },
+    children: 'children 点击复制'
+  }
+}`,...n.parameters?.docs?.source}}};const v=["Basic","ShowIcon","HoverIcon","OnCopied"];export{r as Basic,o as HoverIcon,n as OnCopied,e as ShowIcon,v as __namedExportsOrder,h as default};
---
+++ assets/crud_of_simple.stories-Di6YLoSJ.js
@@ -0,0 +1,87 @@
+import{j as e}from"./jsx-runtime-CWxYxdN8.js";import{a as t}from"./localforage-BzOjR7yL.js";import"./date-CBlPaY01.js";import"./_commonjs-dynamic-modules-CVDP7VWI.js";import"./iframe-BwT8Hu9S.js";import"./isNumber-D2s6sUQp.js";import{f as n,a as u,b as m}from"./data-CaDRzWs7.js";import{W as i}from"./index-CypJDl-q.js";import"./index-r6yxhA4T.js";import"./index-noVmSWaV.js";import"./preload-helper-D9Z9MdNV.js";const B={title:"@fe-free/core/CRUDOfSimple",component:t,tags:["autodocs"]},a={render:()=>{const r=[{title:"名字(省略)",dataIndex:"name",search:!0,ellipsis:!0}];return e.jsx(t,{actions:["create","delete"],tableProps:{columns:r,request:m,pagination:!1},requestDeleteByRecord:u,deleteProps:{nameIndex:"name"},detailForm:()=>e.jsx(e.Fragment,{children:e.jsx(i,{name:"name",label:"名字",required:!0,rules:[{required:!0}],extra:"extra extra extra extra"})}),requestCreateByValues:n})}},s={render:()=>{const r=[{title:"名字(省略)",dataIndex:"name",search:!0,ellipsis:!0}];return e.jsx(t,{actions:["create","delete"],tableProps:{columns:r,request:m,pagination:!1},requestDeleteByRecord:u,deleteProps:{nameIndex:"name"},detailForm:()=>e.jsx(e.Fragment,{children:e.jsx(i,{name:"name",label:"名字",required:!0,rules:[{required:!0}],extra:"extra extra extra extra"})}),requestCreateByValues:n,simpleSearchProps:{name:"name",widthFull:!0}})}},o={render:()=>{const r=[{title:"名字(省略)",dataIndex:"name",search:!0,ellipsis:!0}];return e.jsx(t,{actions:["create","delete"],tableProps:{columns:r,request:m,pagination:!1},requestDeleteByRecord:u,deleteProps:{nameIndex:"name"},detailForm:()=>e.jsx(e.Fragment,{children:e.jsx(i,{name:"name",label:"名字",required:!0,rules:[{required:!0}],extra:"extra extra extra extra"})}),requestCreateByValues:n,simpleOperateHoverShow:!0})}},l={render:()=>{const r=[{title:"名字(省略)",dataIndex:"name",search:!0,ellipsis:!0}];return e.jsx(t,{actions:["delete"],tableProps:{columns:r,request:m,pagination:!1},requestDeleteByRecord:u,deleteProps:{nameIndex:"name"},detailForm:()=>e.jsx(e.Fragment,{children:e.jsx(i,{name:"name",label:"名字",required:!0,rules:[{required:!0}],extra:"extra extra extra extra"})}),requestCreateByValues:n,simpleSearchProps:{name:"name",widthFull:!0}})}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const columns = [{
+      title: '名字(省略)',
+      dataIndex: 'name',
+      search: true,
+      ellipsis: true
+    }];
+    return <CRUDOfSimple actions={['create', 'delete']} tableProps={{
+      columns,
+      request: fakeRequest,
+      pagination: false
+    }} requestDeleteByRecord={fakeDeleteByRecord} deleteProps={{
+      nameIndex: 'name'
+    }} detailForm={() => <>
+            <ProFormText name="name" label="名字" required rules={[{
+        required: true
+      }]} extra="extra extra extra extra" />
+          </>} requestCreateByValues={fakeCreate} />;
+  }
+}`,...a.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const columns = [{
+      title: '名字(省略)',
+      dataIndex: 'name',
+      search: true,
+      ellipsis: true
+    }];
+    return <CRUDOfSimple actions={['create', 'delete']} tableProps={{
+      columns,
+      request: fakeRequest,
+      pagination: false
+    }} requestDeleteByRecord={fakeDeleteByRecord} deleteProps={{
+      nameIndex: 'name'
+    }} detailForm={() => <>
+            <ProFormText name="name" label="名字" required rules={[{
+        required: true
+      }]} extra="extra extra extra extra" />
+          </>} requestCreateByValues={fakeCreate} simpleSearchProps={{
+      name: 'name',
+      widthFull: true
+    }} />;
+  }
+}`,...s.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const columns = [{
+      title: '名字(省略)',
+      dataIndex: 'name',
+      search: true,
+      ellipsis: true
+    }];
+    return <CRUDOfSimple actions={['create', 'delete']} tableProps={{
+      columns,
+      request: fakeRequest,
+      pagination: false
+    }} requestDeleteByRecord={fakeDeleteByRecord} deleteProps={{
+      nameIndex: 'name'
+    }} detailForm={() => <>
+            <ProFormText name="name" label="名字" required rules={[{
+        required: true
+      }]} extra="extra extra extra extra" />
+          </>} requestCreateByValues={fakeCreate} simpleOperateHoverShow />;
+  }
+}`,...o.parameters?.docs?.source}}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const columns = [{
+      title: '名字(省略)',
+      dataIndex: 'name',
+      search: true,
+      ellipsis: true
+    }];
+    return <CRUDOfSimple actions={['delete']} tableProps={{
+      columns,
+      request: fakeRequest,
+      pagination: false
+    }} requestDeleteByRecord={fakeDeleteByRecord} deleteProps={{
+      nameIndex: 'name'
+    }} detailForm={() => <>
+            <ProFormText name="name" label="名字" required rules={[{
+        required: true
+      }]} extra="extra extra extra extra" />
+          </>} requestCreateByValues={fakeCreate} simpleSearchProps={{
+      name: 'name',
+      widthFull: true
+    }} />;
+  }
+}`,...l.parameters?.docs?.source}}};const F=["Normal","WithSearch","HoverShow","JustSearch"];export{o as HoverShow,l as JustSearch,a as Normal,s as WithSearch,F as __namedExportsOrder,B as default};
