16a79ce042072388978ec89b6dfbb83cf1d5b416
liyatang <liyatang@users.noreply.github.com>
2025-09-18 01:52:00 +0000

    deploy: 02363b6872d07031e87de438d363f414086beedc

---
+++ assets/core_app.stories-AQkSleL1.js
@@ -0,0 +1,10 @@
+import{j as o}from"./jsx-runtime-DqKe2-fQ.js";import{a as e}from"./localforage-BZLZtFts.js";import"./environment-BqASjrp8.js";import"./_commonjs-dynamic-modules-D30eG1uD.js";import"./iframe-B0HBeFCP.js";import"./isNumber-B9YZt0gZ.js";import"./index-BVDbDkzM.js";import"./index-B_jVXePx.js";import"./preload-helper-D9Z9MdNV.js";const l={title:"@fe-free/core/CoreApp",component:e,tags:["autodocs"],parameters:{docs:{description:{component:`
+CoreApp 组件
+- 封装常用 ProConfigProvider
+- 封装常用 ConfigProvider
+- 封装常用 APP
+- 封装常用 Router
+- 提供了 @fe-core/app 初始化能力
+`}}}},r={render:()=>o.jsx("div",{children:"hello world"})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: () => <div>hello world</div>
+}`,...r.parameters?.docs?.source}}};const f=["Default"];export{r as Default,f as __namedExportsOrder,l as default};
--- assets/tabs.stories-pOaSTzdh.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./jsx-runtime-DYO6iK1c.js";import{x as t}from"./localforage-BTF3Q14z.js";import"./environment-Bn1z5B45.js";import"./_commonjs-dynamic-modules-BzUgh-JK.js";import"./iframe-C5YgwIi3.js";import"./isNumber-Hrl3mtTe.js";import"./index-CIRwY3z5.js";import"./index-x4HnuTdD.js";import"./preload-helper-D9Z9MdNV.js";const u={title:"@fe-free/core/Tabs",component:t,tags:["autodocs"],parameters:{docs:{description:{component:"基于 antd Tabs 封装的 Tabs，设置 withSearchParams 则自动根据 ?tab= 设置 activeKey"}}}},r={render:()=>e.jsx("div",{children:"依赖 react-router-dom 的 Route 组件，暂不提供 demo"})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: () => {
-    return <div>依赖 react-router-dom 的 Route 组件，暂不提供 demo</div>;
-  }
-}`,...r.parameters?.docs?.source}}};const x=["Basic"];export{r as Basic,x as __namedExportsOrder,u as default};
