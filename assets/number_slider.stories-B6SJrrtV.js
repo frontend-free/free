fff9185867aeb637c1a8c74f1432a3770f3c437a
liyatang <liyatang@users.noreply.github.com>
2025-09-26 08:11:01 +0000

    deploy: 18ce5ce9313c8850c8858776326a19f95eb32b1c

---
+++ assets/number_slider.stories-B6SJrrtV.js
@@ -0,0 +1,11 @@
+import{N as s,r as c,j as d}from"./iframe-DIttv1M3.js";import"./preload-helper-D9Z9MdNV.js";const i={title:"@fe-free/core/NumberSlider",component:s,tags:["autodocs"]};function a(n){const[o,t]=c.useState(0);return d.jsx(s,{value:o,onChange:t,...n})}const e={args:{},render:a},r={args:{max:1,step:.1,precision:1},render:a};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {},
+  render: Render
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    max: 1,
+    step: 0.1,
+    precision: 1
+  },
+  render: Render
+}`,...r.parameters?.docs?.source}}};const u=["Basic","One"];export{e as Basic,r as One,u as __namedExportsOrder,i as default};
