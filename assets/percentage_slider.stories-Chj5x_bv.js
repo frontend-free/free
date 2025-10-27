c7e2e95cfd0e7bb7351aa868c586800ba75d17ba
liyatang <liyatang@users.noreply.github.com>
2025-10-27 02:56:59 +0000

    deploy: 2adda8978e0af95ae11204554f2ce32cea839223

---
+++ assets/percentage_slider.stories-Chj5x_bv.js
@@ -0,0 +1,4 @@
+import{x as r,r as o,j as n}from"./iframe-D0OdbHn2.js";import"./preload-helper-D9Z9MdNV.js";const m={title:"@fe-free/core/PercentageSlider",component:r,tags:["autodocs"]};function c(s){const[t,a]=o.useState([10,30,60]);return n.jsx(r,{value:t,onChange:a,...s})}const e={args:{},render:c};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {},
+  render: Render
+}`,...e.parameters?.docs?.source}}};const p=["Basic"];export{e as Basic,p as __namedExportsOrder,m as default};
--- assets/tabs.stories-Dclavaoj.js
+++
@@ -1,5 +0,0 @@
-import{z as r,j as t}from"./iframe-BMRDrK3c.js";import"./preload-helper-D9Z9MdNV.js";const s={title:"@fe-free/core/Tabs",component:r,tags:["autodocs"],parameters:{docs:{description:{component:"基于 antd Tabs 封装的 Tabs，设置 withSearchParams 则自动根据 ?tab= 设置 activeKey"}}}},e={render:()=>t.jsx("div",{children:"依赖 react-router-dom 的 Route 组件，暂不提供 demo"})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  render: () => {
-    return <div>依赖 react-router-dom 的 Route 组件，暂不提供 demo</div>;
-  }
-}`,...e.parameters?.docs?.source}}};const c=["Basic"];export{e as Basic,c as __namedExportsOrder,s as default};
