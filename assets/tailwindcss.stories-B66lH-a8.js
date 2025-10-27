d3eec03e3a034e9fa8a041bd70cc1b4d9c10c3d7
liyatang <liyatang@users.noreply.github.com>
2025-10-27 12:52:09 +0000

    deploy: 74d6c86dd6e3d26b5d2ac3ba50230fca463594e4

---
+++ assets/tailwindcss.stories-B66lH-a8.js
@@ -0,0 +1,18 @@
+import{j as e}from"./iframe-k2hI8NkN.js";import"./preload-helper-D9Z9MdNV.js";const c={title:"@fe-free/core/tailwindcss",tags:["autodocs"]},r=()=>e.jsxs("div",{children:[e.jsx("div",{children:"@fe-free/core 扩展的"}),e.jsxs("div",{children:[e.jsx("div",{className:"text-primary",children:"text-primary"}),e.jsx("div",{className:"text-secondary",children:"text-secondary"}),e.jsx("div",{className:"text-desc",children:"text-desc"})]}),e.jsxs("div",{children:[e.jsx("div",{className:"fec-border-bottom",children:"fec-border-bottom"}),e.jsx("div",{className:"fec-border-top",children:"fec-border-top"}),e.jsx("div",{className:"fec-border-left",children:"fec-border-left"}),e.jsx("div",{className:"fec-border-right",children:"fec-border-right"}),e.jsx("div",{className:"fec-bg",children:"fec-bg"}),e.jsx("div",{className:"fec-bg-hover",children:"fec-bg-hover"})]})]});r.__docgenInfo={description:"",methods:[],displayName:"Default"};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`() => {
+  return <div>
+      <div>@fe-free/core 扩展的</div>
+      <div>
+        <div className="text-primary">text-primary</div>
+        <div className="text-secondary">text-secondary</div>
+        <div className="text-desc">text-desc</div>
+      </div>
+      <div>
+        <div className="fec-border-bottom">fec-border-bottom</div>
+        <div className="fec-border-top">fec-border-top</div>
+        <div className="fec-border-left">fec-border-left</div>
+        <div className="fec-border-right">fec-border-right</div>
+        <div className="fec-bg">fec-bg</div>
+        <div className="fec-bg-hover">fec-bg-hover</div>
+      </div>
+    </div>;
+}`,...r.parameters?.docs?.source}}};const i=["Default"];export{r as Default,i as __namedExportsOrder,c as default};
