1174baafd71c9777ef9ef24d99323ea672295564
liyatang <liyatang@users.noreply.github.com>
2025-11-19 04:51:13 +0000

    deploy: 693df4e33f9244a7e1a484e1d412b003d3745c63

---
+++ assets/table.stories-Dv1oWG7v.js
@@ -0,0 +1,32 @@
+import{T as r,j as t}from"./iframe-CWWzaUCv.js";import{c as s}from"./data-fAvIreVl.js";import"./preload-helper-PPVm8Dsz.js";const l={title:"@fe-free/core/Table",component:r,tags:["autodocs"],parameters:{docs:{description:{component:`Table 基于 ProTable 做了一些封装：<br>
+- props column 需要显示的提供 search: true;
+- 列宽默认 120，和滚动条
+- 搜索样式做了默认设置，具体见代码
+- 页码做了默认设置，具体见代码
+`}}}},n=[{title:"id",dataIndex:"id",search:!0},{title:"名字(省略)",dataIndex:"name",search:!0,ellipsis:!0},{title:"city",dataIndex:"city"},{title:"area",dataIndex:"area"}],e={render:()=>t.jsx(r,{rowKey:"id",columns:n,request:async o=>(console.log(o),{data:s,success:!0,total:s.length})})},a={render:()=>t.jsx("div",{className:"w-[400px]",children:t.jsx(r,{rowKey:"id",columns:[{title:"id",dataIndex:"id"},{title:"名字(省略)",dataIndex:"name",ellipsis:!0},{title:"city",dataIndex:"city"},{title:"area",dataIndex:"area"}]})})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: () => <Table rowKey="id" columns={columns} request={async params => {
+    console.log(params);
+    return {
+      data: fakeData,
+      success: true,
+      total: fakeData.length
+    };
+  }} />
+}`,...e.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  render: () => <div className="w-[400px]">
+      <Table rowKey="id" columns={[{
+      title: 'id',
+      dataIndex: 'id'
+    }, {
+      title: '名字(省略)',
+      dataIndex: 'name',
+      ellipsis: true
+    }, {
+      title: 'city',
+      dataIndex: 'city'
+    }, {
+      title: 'area',
+      dataIndex: 'area'
+    }]} />
+    </div>
+}`,...a.parameters?.docs?.source}}};const m=["Basic","ScrollX"];export{e as Basic,a as ScrollX,m as __namedExportsOrder,l as default};
