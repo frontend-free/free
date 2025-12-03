e65d5e02a291696dbea6a700479ee754d883e150
liyatang <liyatang@users.noreply.github.com>
2025-12-03 10:47:14 +0000

    deploy: 37592f9e36518d098ad4341bf398ccf712619cf4

---
+++ assets/editor_logs.stories-Bx5nZgKh.js
@@ -0,0 +1,51 @@
+import{f as g,j as a}from"./iframe-oomJbY8O.js";import"./preload-helper-PPVm8Dsz.js";const r={title:"@fe-free/core/EditorLogs",component:g,tags:["autodocs"],parameters:{docs:{description:{component:"一个用于显示日志内容的 React 组件。它使用 CodeMirror 编辑器来呈现日志，提供了语法高亮和主题支持。"}}}},s={args:{logs:[{timestamp:"2023-01-01 12:00:00",level:"info",message:"This is an info log message."},{timestamp:"2023-01-01 12:00:00",level:"warn",message:"This is a warning log message."},{timestamp:"2023-01-01 12:00:00",level:"error",message:"This is an error log message."},{timestamp:"2023-01-01 12:00:00",level:"system",message:"This is a debug log message. This is a debug log message This is a debug log message This is a debug log message This is a debug log message"},{timestamp:"2023-01-01 12:00:00",message:"This is an log message."},{level:"info",message:"This is an log message."},{message:"This is an log message."}]},render:i=>a.jsx("div",{style:{width:"500px",height:"500px"},children:a.jsx(g,{...i})})},e={args:{logs:[{message:"This is an info log message."},{message:"This is a warning log message."},{message:"This is an error log message."}]},render:i=>a.jsx("div",{style:{width:"500px",height:"500px"},children:a.jsx(g,{...i})})};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    logs: [{
+      timestamp: '2023-01-01 12:00:00',
+      level: 'info',
+      message: 'This is an info log message.'
+    }, {
+      timestamp: '2023-01-01 12:00:00',
+      level: 'warn',
+      message: 'This is a warning log message.'
+    }, {
+      timestamp: '2023-01-01 12:00:00',
+      level: 'error',
+      message: 'This is an error log message.'
+    }, {
+      timestamp: '2023-01-01 12:00:00',
+      level: 'system',
+      message: 'This is a debug log message. This is a debug log message This is a debug log message This is a debug log message This is a debug log message'
+    }, {
+      timestamp: '2023-01-01 12:00:00',
+      message: 'This is an log message.'
+    }, {
+      level: 'info',
+      message: 'This is an log message.'
+    }, {
+      message: 'This is an log message.'
+    }]
+  },
+  render: props => <div style={{
+    width: '500px',
+    height: '500px'
+  }}>
+      <EditorLogs {...props} />
+    </div>
+}`,...s.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    logs: [{
+      message: 'This is an info log message.'
+    }, {
+      message: 'This is a warning log message.'
+    }, {
+      message: 'This is an error log message.'
+    }]
+  },
+  render: props => <div style={{
+    width: '500px',
+    height: '500px'
+  }}>
+      <EditorLogs {...props} />
+    </div>
+}`,...e.parameters?.docs?.source}}};const m=["Basic","Pure"];export{s as Basic,e as Pure,m as __namedExportsOrder,r as default};
