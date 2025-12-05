2ed6a7f2ad65245ef5c0bd72444be2aacb01a774
liyatang <liyatang@users.noreply.github.com>
2025-12-05 09:55:29 +0000

    deploy: 413bf40692039ac25005dcfe1c06ff60a162e1b2

---
+++ assets/tabs.stories-C1vu34Aq.js
@@ -0,0 +1,5 @@
+import{T as r,j as t}from"./iframe-DqikbrxM.js";import"./preload-helper-PPVm8Dsz.js";const s={title:"@fe-free/core/Tabs",component:r,tags:["autodocs"],parameters:{docs:{description:{component:"基于 antd Tabs 封装的 Tabs，设置 withSearchParams 则自动根据 ?tab= 设置 activeKey"}}}},e={render:()=>t.jsx("div",{children:"依赖 react-router-dom 的 Route 组件，暂不提供 demo"})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    return <div>依赖 react-router-dom 的 Route 组件，暂不提供 demo</div>;
+  }
+}`,...e.parameters?.docs?.source}}};const c=["Basic"];export{e as Basic,c as __namedExportsOrder,s as default};
