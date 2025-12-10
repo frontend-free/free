3f0cca000f81452ab96ea8860657da3035967de0
liyatang <liyatang@users.noreply.github.com>
2025-12-10 11:53:57 +0000

    deploy: 25f63c2dd9d833c4d4ca64a363a969741c2b9e7a

---
+++ assets/crud.stories-CMuJA9Eb.js
@@ -0,0 +1,335 @@
+import{R as S,_ as T,j as e,U as w,V as F,O as a,W as d,B as k,r as D,h as U}from"./iframe-BZhXqAvG.js";import{a as G}from"./filter-CvpFJvOc.js";import{c as o,f as u,d as c,a as l,b as s,l as V,e as A,g as _,h as E}from"./data-CBTlBabw.js";import"./preload-helper-PPVm8Dsz.js";var N=["fieldProps","unCheckedChildren","checkedChildren","proFieldProps"],O=S.forwardRef(function(r,t){var n=r.fieldProps,i=r.unCheckedChildren,j=r.checkedChildren,g=r.proFieldProps,v=T(r,N);return e.jsx(w,F({valueType:"switch",fieldProps:F({unCheckedChildren:i,checkedChildren:j},n),ref:t,valuePropName:"checked",proFieldProps:g,filedConfig:{valuePropName:"checked",ignoreWidth:!0,customLightMode:!0}},v))});const m={fieldProps:{showSearch:!0,filterOption:(r,t)=>G(t.label,r),fetchDataOnSearch:!1}},J={title:"@fe-free/core/CRUD",component:a,tags:["autodocs"]},p={render:()=>{const r=[{title:"id",dataIndex:"id",search:!0},{title:"名字(省略)",dataIndex:"name",search:!0,ellipsis:!0},{title:"city",dataIndex:"city"},{title:"area",dataIndex:"area"}];return e.jsx(a,{actions:["create","read","delete","update"],tableProps:{columns:r,request:s},requestDeleteByRecord:l,deleteProps:{nameIndex:"name"},detailForm:()=>e.jsx(e.Fragment,{children:e.jsx(d,{name:"name",label:"名字",required:!0,rules:[{required:!0}],extra:"extra extra extra extra"})}),requestGetByRecord:c,requestCreateByValues:u,requestUpdateById:o})}},x={render:()=>{const r=[{title:"id",dataIndex:"id",search:!0},{title:"名字",dataIndex:"name",search:!0}];return e.jsx(a,{actions:["read_detail"],tableProps:{columns:r,request:s}})}},h={render:()=>e.jsx(a,{actions:["batch_delete"],tableProps:{columns:[{title:"id",dataIndex:"id",search:!0},{title:"名字",dataIndex:"name",search:!0}],request:s},requestDeleteByRecords:async r=>{console.log(r)}})},y={render:()=>{const r=[{title:"id",dataIndex:"id",search:!0},{title:"名字(省略)",dataIndex:"name",search:!0,ellipsis:!0},{title:"city",dataIndex:"city"},{title:"area",dataIndex:"area"}];return e.jsx(a,{actions:["create","read","delete","update"],tableProps:{columns:r,request:s,toolBarRender:()=>[e.jsx("div",{children:"自定义1"},"custom1"),e.jsx("div",{children:"自定义2"},"custom2")]},operateColumnProps:{width:300,moreOperator:()=>e.jsx("div",{children:"自定义"}),moreOperatorAfter:()=>e.jsx("div",{children:"自定义"})},createButton:e.jsx(k,{type:"primary",children:"自定义新建文本"}),readProps:{operateIsDisabled:t=>!(t.id%3)},requestDeleteByRecord:l,deleteProps:{nameIndex:"name",operateIsDisabled:t=>!(t.id%3)},detailForm:()=>e.jsx(e.Fragment,{children:e.jsx(d,{name:"name",label:"名字",required:!0,rules:[{required:!0}],extra:"extra extra extra extra"})}),requestGetByRecord:c,requestCreateByValues:u,requestUpdateById:o,updateProps:{operateIsDisabled:t=>!(t.id%3),operateIsHidden:t=>!(t.id%4)}})}},K=()=>{const r=D.useRef(),[t]=U.useForm(),n=[{title:"id",dataIndex:"id",search:!0},{title:"名字",dataIndex:"name",search:!0}];return e.jsx(a,{actions:["create","read","update"],tableProps:{formRef:r,columns:n,request:s},detailFormInstance:t,detailForm:()=>e.jsxs(e.Fragment,{children:[e.jsx(d,{name:"name",label:"名字",required:!0,rules:[{required:!0}],initialValue:"default"}),e.jsx(O,{name:"status",label:"开启",initialValue:!1})]}),requestGetByRecord:c,requestCreateByValues:u,requestUpdateByValues:o})},q={render:()=>e.jsx(K,{})},M=()=>{const r=D.useRef(null),t=[{title:"id",dataIndex:"id",search:!0},{title:"名字",dataIndex:"name",search:!0}];return e.jsxs(e.Fragment,{children:[e.jsx(k,{onClick:()=>r.current?.getActionRef()?.current?.reload(),children:"reload"}),e.jsx(a,{ref:r,actions:[],tableProps:{columns:t,request:s}})]})},R={render:()=>e.jsx(M,{})},f={render:()=>{const r=[{title:"id",dataIndex:"id",search:!0},{title:"名字",dataIndex:"name",search:!0},{title:"等级(本地数据)",dataIndex:"level",search:!0,valueEnum:V,...m},{title:"city(远端数据)",dataIndex:"city",search:!0,request:async()=>(await _()).map(n=>({label:n,value:n})),...m},{title:"area(联动 city)",dataIndex:"area",search:!0,request:async t=>(await E(t)).map(i=>({label:i,value:i})),dependencies:["city"],...m},{title:"学校(远端数据 label value)",dataIndex:"school",search:!0,valueType:"select",request:()=>A(),...m}];return e.jsx(a,{actions:[],tableProps:{columns:r,request:s}})}},I={render:()=>{const r=[{title:"id",dataIndex:"id"},{title:"名字",dataIndex:"name"}];return e.jsx(a,{actions:[],tableProps:{columns:r,request:s,search:!1}})}},B={render:()=>{const r=[{title:"id",dataIndex:"id",search:!0},{title:"名字",dataIndex:"name",search:!0}];return e.jsx(a,{actions:["create","read","delete","update"],tableProps:{columns:r,request:s},createButton:e.jsx(k,{type:"primary",children:"新建"}),createProps:{submitText:"自定义新建确定",resetText:"自定义新建取消"},readProps:{submitText:"自定义查看确定",resetText:"自定义查看取消",operateText:"查看"},deleteProps:{nameIndex:"name",operateText:"删除",desc:"确定要删除吗？"},updateProps:{operateText:"编辑",submitText:"自定义编辑确定",resetText:"自定义编辑取消",successText:"编辑成功"},requestDeleteByRecord:l,detailForm:()=>e.jsx(e.Fragment,{children:e.jsx(d,{name:"name",label:"名字",required:!0,rules:[{required:!0}]})}),requestGetByRecord:c,requestCreateByValues:u,requestUpdateByValues:o})}},P={render:()=>e.jsx(a,{actions:[],tableProps:{columns:[{title:"id",dataIndex:"id",search:!0},{title:"名字",dataIndex:"name",search:!0}],request:s,rowSelection:{alwaysShowAlert:!0}},batchActions:[{btnText:"批量xxx",onClick:async(r,{selectedRowKeys:t})=>{console.log(t)}},{btnText:"批量danger",danger:!0,onClick:async(r,{selectedRowKeys:t})=>{console.log(t)}}]})},b={render:()=>{const r=[{title:"id",dataIndex:"id",search:!0},{title:"名字(省略)",dataIndex:"name",search:!0,ellipsis:!0}];return e.jsx(a,{actions:["create","read","delete","update"],tableProps:{columns:r,request:s,defaultExpandAllRows:!0,expandable:{expandedRowKeys:["0","1"],expandedRowRender:()=>e.jsx("div",{children:"123"})}},requestDeleteByRecord:l,deleteProps:{nameIndex:"name"},detailForm:()=>e.jsx(e.Fragment,{children:e.jsx(d,{name:"name",label:"名字",required:!0,rules:[{required:!0}],extra:"extra extra extra extra"})}),requestGetByRecord:c,requestCreateByValues:u,requestUpdateById:o})}},C={render:()=>{const r=[{title:"id",dataIndex:"id",search:!0},{title:"名字(省略)",dataIndex:"name",search:!0,ellipsis:!0},{title:"city",dataIndex:"city"},{title:"area",dataIndex:"area"}];return e.jsx("div",{className:"h-[800px] border border-red-500",children:e.jsx(a,{fullPage:!0,actions:["create","read","delete","update"],tableProps:{columns:r,request:s},requestDeleteByRecord:l,deleteProps:{nameIndex:"name"},detailForm:()=>e.jsx(e.Fragment,{children:e.jsx(d,{name:"name",label:"名字",required:!0,rules:[{required:!0}],extra:"extra extra extra extra"})}),requestGetByRecord:c,requestCreateByValues:u,requestUpdateById:o})})}};p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const columns = [{
+      title: 'id',
+      dataIndex: 'id',
+      search: true
+    }, {
+      title: '名字(省略)',
+      dataIndex: 'name',
+      search: true,
+      ellipsis: true
+    }, {
+      title: 'city',
+      dataIndex: 'city'
+    }, {
+      title: 'area',
+      dataIndex: 'area'
+    }];
+    return <CRUD actions={['create', 'read', 'delete', 'update']} tableProps={{
+      columns,
+      request: fakeRequest
+    }} requestDeleteByRecord={fakeDeleteByRecord} deleteProps={{
+      nameIndex: 'name'
+    }} detailForm={() => <>
+            <ProFormText name="name" label="名字" required rules={[{
+        required: true
+      }]} extra="extra extra extra extra" />
+          </>} requestGetByRecord={fakeGetByRecord} requestCreateByValues={fakeCreate} requestUpdateById={fakeUpdateById} />;
+  }
+}`,...p.parameters?.docs?.source}}};x.parameters={...x.parameters,docs:{...x.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const columns = [{
+      title: 'id',
+      dataIndex: 'id',
+      search: true
+    }, {
+      title: '名字',
+      dataIndex: 'name',
+      search: true
+    }];
+    return <CRUD actions={['read_detail']} tableProps={{
+      columns,
+      request: fakeRequest
+    }} />;
+  }
+}`,...x.parameters?.docs?.source}}};h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    return <CRUD actions={['batch_delete']} tableProps={{
+      columns: [{
+        title: 'id',
+        dataIndex: 'id',
+        search: true
+      }, {
+        title: '名字',
+        dataIndex: 'name',
+        search: true
+      }],
+      request: fakeRequest
+    }} requestDeleteByRecords={async records => {
+      console.log(records);
+    }} />;
+  }
+}`,...h.parameters?.docs?.source}}};y.parameters={...y.parameters,docs:{...y.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const columns = [{
+      title: 'id',
+      dataIndex: 'id',
+      search: true
+    }, {
+      title: '名字(省略)',
+      dataIndex: 'name',
+      search: true,
+      ellipsis: true
+    }, {
+      title: 'city',
+      dataIndex: 'city'
+    }, {
+      title: 'area',
+      dataIndex: 'area'
+    }];
+    return <CRUD actions={['create', 'read', 'delete', 'update']} tableProps={{
+      columns,
+      request: fakeRequest,
+      toolBarRender: () => {
+        return [<div key="custom1">自定义1</div>, <div key="custom2">自定义2</div>];
+      }
+    }} operateColumnProps={{
+      // 自定义宽度
+      width: 300,
+      // 自定义操作列
+      moreOperator: () => {
+        return <div>自定义</div>;
+      },
+      // 自定义操作列之后
+      moreOperatorAfter: () => {
+        return <div>自定义</div>;
+      }
+    }} createButton={<Button type="primary">自定义新建文本</Button>} readProps={{
+      operateIsDisabled: record => {
+        if (record.id % 3) {
+          return false;
+        }
+        return true;
+      }
+    }} requestDeleteByRecord={fakeDeleteByRecord} deleteProps={{
+      nameIndex: 'name',
+      operateIsDisabled: record => {
+        if (record.id % 3) {
+          return false;
+        }
+        return true;
+      }
+    }} detailForm={() => <>
+            <ProFormText name="name" label="名字" required rules={[{
+        required: true
+      }]} extra="extra extra extra extra" />
+          </>} requestGetByRecord={fakeGetByRecord} requestCreateByValues={fakeCreate} requestUpdateById={fakeUpdateById} updateProps={{
+      operateIsDisabled: record => {
+        if (record.id % 3) {
+          return false;
+        }
+        return true;
+      },
+      operateIsHidden: record => {
+        if (record.id % 4) {
+          return false;
+        }
+        return true;
+      }
+    }} />;
+  }
+}`,...y.parameters?.docs?.source}}};q.parameters={...q.parameters,docs:{...q.parameters?.docs,source:{originalSource:`{
+  render: () => <FormRefComponent />
+}`,...q.parameters?.docs?.source}}};R.parameters={...R.parameters,docs:{...R.parameters?.docs,source:{originalSource:`{
+  render: () => <ActionRefComponent />
+}`,...R.parameters?.docs?.source}}};f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const columns: ProColumns<any>[] = [{
+      title: 'id',
+      dataIndex: 'id',
+      search: true
+    }, {
+      title: '名字',
+      dataIndex: 'name',
+      search: true
+    }, {
+      title: '等级(本地数据)',
+      dataIndex: 'level',
+      search: true,
+      valueEnum: levels,
+      ...proFormSelectSearchProps
+    }, {
+      title: 'city(远端数据)',
+      dataIndex: 'city',
+      search: true,
+      request: async () => {
+        const res = await fakeRequestCity();
+        return res.map(item => ({
+          label: item,
+          value: item
+        }));
+      },
+      ...proFormSelectSearchProps
+    }, {
+      title: 'area(联动 city)',
+      dataIndex: 'area',
+      search: true,
+      request: async params => {
+        const res = await fakeRequestArea(params);
+        return res.map(item => ({
+          label: item,
+          value: item
+        }));
+      },
+      dependencies: ['city'],
+      ...proFormSelectSearchProps
+    }, {
+      title: '学校(远端数据 label value)',
+      dataIndex: 'school',
+      search: true,
+      valueType: 'select' as const,
+      request: () => fakeRequestSchool(),
+      ...proFormSelectSearchProps
+    }];
+    return <CRUD actions={[]} tableProps={{
+      columns,
+      request: fakeRequest
+    }} />;
+  }
+}`,...f.parameters?.docs?.source}}};I.parameters={...I.parameters,docs:{...I.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const columns = [{
+      title: 'id',
+      dataIndex: 'id'
+    }, {
+      title: '名字',
+      dataIndex: 'name'
+    }];
+    return <CRUD actions={[]} tableProps={{
+      columns,
+      request: fakeRequest,
+      search: false
+    }} />;
+  }
+}`,...I.parameters?.docs?.source}}};B.parameters={...B.parameters,docs:{...B.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const columns = [{
+      title: 'id',
+      dataIndex: 'id',
+      search: true
+    }, {
+      title: '名字',
+      dataIndex: 'name',
+      search: true
+    }];
+    return <CRUD actions={['create', 'read', 'delete', 'update']} tableProps={{
+      columns,
+      request: fakeRequest
+    }} createButton={<Button type="primary">新建</Button>} createProps={{
+      submitText: '自定义新建确定',
+      resetText: '自定义新建取消'
+    }} readProps={{
+      submitText: '自定义查看确定',
+      resetText: '自定义查看取消',
+      operateText: '查看'
+    }} deleteProps={{
+      nameIndex: 'name',
+      operateText: '删除',
+      desc: '确定要删除吗？'
+    }} updateProps={{
+      operateText: '编辑',
+      submitText: '自定义编辑确定',
+      resetText: '自定义编辑取消',
+      successText: '编辑成功'
+    }} requestDeleteByRecord={fakeDeleteByRecord} detailForm={() => <>
+            <ProFormText name="name" label="名字" required rules={[{
+        required: true
+      }]} />
+          </>} requestGetByRecord={fakeGetByRecord} requestCreateByValues={fakeCreate} requestUpdateByValues={fakeUpdateById} />;
+  }
+}`,...B.parameters?.docs?.source}}};P.parameters={...P.parameters,docs:{...P.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    return <CRUD actions={[]} tableProps={{
+      columns: [{
+        title: 'id',
+        dataIndex: 'id',
+        search: true
+      }, {
+        title: '名字',
+        dataIndex: 'name',
+        search: true
+      }],
+      request: fakeRequest,
+      rowSelection: {
+        alwaysShowAlert: true
+      }
+    }} batchActions={[{
+      btnText: '批量xxx',
+      onClick: async (_, {
+        selectedRowKeys
+      }) => {
+        console.log(selectedRowKeys);
+      }
+    }, {
+      btnText: '批量danger',
+      danger: true,
+      onClick: async (_, {
+        selectedRowKeys
+      }) => {
+        console.log(selectedRowKeys);
+      }
+    }]} />;
+  }
+}`,...P.parameters?.docs?.source}}};b.parameters={...b.parameters,docs:{...b.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const columns = [{
+      title: 'id',
+      dataIndex: 'id',
+      search: true
+    }, {
+      title: '名字(省略)',
+      dataIndex: 'name',
+      search: true,
+      ellipsis: true
+    }];
+    return <CRUD actions={['create', 'read', 'delete', 'update']} tableProps={{
+      columns,
+      request: fakeRequest,
+      defaultExpandAllRows: true,
+      expandable: {
+        expandedRowKeys: ['0', '1'],
+        expandedRowRender: () => {
+          return <div>123</div>;
+        }
+      }
+    }} requestDeleteByRecord={fakeDeleteByRecord} deleteProps={{
+      nameIndex: 'name'
+    }} detailForm={() => <>
+            <ProFormText name="name" label="名字" required rules={[{
+        required: true
+      }]} extra="extra extra extra extra" />
+          </>} requestGetByRecord={fakeGetByRecord} requestCreateByValues={fakeCreate} requestUpdateById={fakeUpdateById} />;
+  }
+}`,...b.parameters?.docs?.source}}};C.parameters={...C.parameters,docs:{...C.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const columns = [{
+      title: 'id',
+      dataIndex: 'id',
+      search: true
+    }, {
+      title: '名字(省略)',
+      dataIndex: 'name',
+      search: true,
+      ellipsis: true
+    }, {
+      title: 'city',
+      dataIndex: 'city'
+    }, {
+      title: 'area',
+      dataIndex: 'area'
+    }];
+    return <div className="h-[800px] border border-red-500">
+        <CRUD fullPage actions={['create', 'read', 'delete', 'update']} tableProps={{
+        columns,
+        request: fakeRequest
+      }} requestDeleteByRecord={fakeDeleteByRecord} deleteProps={{
+        nameIndex: 'name'
+      }} detailForm={() => <>
+              <ProFormText name="name" label="名字" required rules={[{
+          required: true
+        }]} extra="extra extra extra extra" />
+            </>} requestGetByRecord={fakeGetByRecord} requestCreateByValues={fakeCreate} requestUpdateById={fakeUpdateById} />
+      </div>;
+  }
+}`,...C.parameters?.docs?.source}}};const Q=["Normal","ReadDetail","BatchDelete","MoreCustom","FormRef","ActionRef","RemoteData","NoSearch","CustomText","RowSelection","ExpandedRowRender","FullPage"];export{R as ActionRef,h as BatchDelete,B as CustomText,b as ExpandedRowRender,q as FormRef,C as FullPage,y as MoreCustom,I as NoSearch,p as Normal,x as ReadDetail,f as RemoteData,P as RowSelection,Q as __namedExportsOrder,J as default};
