98287e38c483651165446d9992321811e2fc7d7f
liyatang <liyatang@users.noreply.github.com>
2025-12-12 14:22:27 +0000

    deploy: b5c8180ee8bd599ffc0d308bceb58b58ed54127c

---
+++ assets/icons.stories-CNukiUcG.js
@@ -0,0 +1,79 @@
+import{R as M,j as t,r as L,ak as V,al as H}from"./iframe-DVVmkQul.js";import"./preload-helper-PPVm8Dsz.js";function O(s){const e=s.match(/viewBox=["']([^"']+)["']/);return e?e[1]:"0 0 1024 1024"}function Z(s){let e=s.replace(/<svg[^>]*>/,"").replace(/<\/svg>/,"").trim();return e=e.replace(/fill="[^"]*"/g,'fill="currentColor"'),e}function n(s,e){const l=O(e),c=Z(e),i=M.forwardRef((d,C)=>{const{size:g="1em",spin:w=!1,rotate:p,style:v,className:u,viewBox:m,...x}=d,f={display:"inline-block",...v,...p?{transform:`rotate(${p}deg)`}:{},...w?{animation:"anticon-spin 1s infinite linear"}:{}};return t.jsx("svg",{viewBox:m||l,width:g,height:g,fill:"currentColor",className:u,style:f,ref:C,...x,dangerouslySetInnerHTML:{__html:c}})});return i.displayName=s,i}const y=`<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M16.0608 5.99999L10.0608 12L16.0608 18L15.0001 19.0607L7.93945 12L15.0001 4.93933L16.0608 5.99999Z" fill="#222222"/>
+</svg>`,B=n("AngleLeftOutlined",y),j=`<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M11 18.5625C15.1767 18.5625 18.5625 15.1767 18.5625 11C18.5625 6.82335 15.1767 3.4375 11 3.4375C6.82335 3.4375 3.4375 6.82335 3.4375 11C3.4375 15.1767 6.82335 18.5625 11 18.5625ZM11 19.9375C15.936 19.9375 19.9375 15.936 19.9375 11C19.9375 6.06395 15.936 2.0625 11 2.0625C6.06395 2.0625 2.0625 6.06395 2.0625 11C2.0625 15.936 6.06395 19.9375 11 19.9375Z" fill="#777777"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M14.4028 7.59708L12.9444 12.9446L7.59689 14.403L9.0553 9.05548L14.4028 7.59708ZM12.4431 9.55675L10.1751 10.1753L9.55656 12.4433L11.8246 11.8248L12.4431 9.55675Z" fill="#777777"/>
+</svg>`,b=n("CompassOutlined",j),E=`<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_1199_4431)">
+<path d="M6 19.5V21H3.75V19.5H6ZM6 11.25H3.75V21L3.59692 20.9919C2.89076 20.9204 2.32965 20.3592 2.25806 19.6531L2.25 19.5V11.25C2.25 10.4216 2.92157 9.75 3.75 9.75H6C6.82843 9.75 7.5 10.4216 7.5 11.25V19.5C7.5 20.2767 6.90958 20.9152 6.15308 20.9919L6 21V11.25Z" fill="#777777"/>
+<path d="M13.5 19.5V21H10.5V19.5H13.5ZM13.5 7.5H10.5V21C9.72326 21 9.08475 20.4096 9.00806 19.6531L9 19.5V7.5C9 6.67157 9.67157 6 10.5 6H13.5L13.6531 6.00806C14.4096 6.08475 15 6.72326 15 7.5V19.5L14.9919 19.6531C14.9152 20.4096 14.2767 21 13.5 21V7.5Z" fill="#777777"/>
+<path d="M20.25 19.5V21H18V19.5H20.25ZM20.25 3.75H18V21L17.8469 20.9919C17.0904 20.9152 16.5 20.2767 16.5 19.5V3.75C16.5 2.92157 17.1716 2.25 18 2.25H20.25C21.0784 2.25 21.75 2.92157 21.75 3.75V19.5C21.75 20.2767 21.1596 20.9152 20.4031 20.9919L20.25 21V3.75Z" fill="#777777"/>
+</g>
+<defs>
+<clipPath id="clip0_1199_4431">
+<rect width="24" height="24" fill="white"/>
+</clipPath>
+</defs>
+</svg>`,N=n("DatasetOutlined",E),_=`<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12.2236 1.875C12.7616 1.875 13.24 2.21913 13.4102 2.72949L13.958 4.375H17.5V5.625H15.5654L15.1133 15.1191C15.0497 16.452 13.9506 17.5 12.6162 17.5H7.38379C6.04939 17.5 4.95031 16.452 4.88672 15.1191L4.43457 5.625H2.5V4.375H6.04199L6.58984 2.72949C6.76002 2.21913 7.23837 1.875 7.77637 1.875H12.2236ZM6.13477 15.0596C6.16655 15.726 6.71658 16.25 7.38379 16.25H12.6162C13.2834 16.25 13.8335 15.726 13.8652 15.0596L14.3145 5.625H5.68555L6.13477 15.0596ZM9.06152 14.3486L7.8125 14.4004L7.52539 7.5H8.77637L9.06152 14.3486ZM12.1865 14.4004L10.9375 14.3486L11.2227 7.5H12.4736L12.1865 14.4004ZM7.35938 4.375H12.6406L12.2236 3.125H7.77637L7.35938 4.375Z" fill="#0374E9"/>
+</svg>`,o=n("DeleteOutlined",_),I=`<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M9.37498 3.75H10.625V13.9461L13.5355 11.0355L14.4194 11.9194L9.99999 16.3388L5.58057 11.9194L6.46445 11.0355L9.37498 13.9461V3.75Z" fill="#0374E9"/>
+</svg>`,D=n("DownloadOutlined",I),S=`<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M15.9663 5.33471C16.6985 6.06694 16.6985 7.25413 15.9663 7.98636L8.01618 15.9365L4.47582 15.9413L4.48065 12.4009L12.4308 4.45083C13.163 3.71859 14.3502 3.71859 15.0824 4.45083L15.9663 5.33471ZM5.72983 12.9195L5.72775 14.6894L7.49759 14.6873L13.3147 8.87024L11.5469 7.10248L5.72983 12.9195ZM14.1985 5.33471C13.9545 5.09063 13.5587 5.09063 13.3147 5.33471L12.4308 6.21859L14.1985 7.98636L15.0824 7.10248C15.3265 6.8584 15.3265 6.46267 15.0824 6.21859L14.1985 5.33471Z" fill="#0374E9"/>
+</svg>`,$=n("EditOutlined",S),P=`<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_1199_4516)">
+<path d="M20.25 12C20.25 7.44365 16.5563 3.75 12 3.75C7.44365 3.75 3.75 7.44365 3.75 12C3.75 16.5563 7.44365 20.25 12 20.25V21.75C6.61522 21.75 2.25 17.3848 2.25 12C2.25 6.61522 6.61522 2.25 12 2.25C17.3848 2.25 21.75 6.61522 21.75 12C21.75 17.3848 17.3848 21.75 12 21.75V20.25C16.5563 20.25 20.25 16.5563 20.25 12Z" fill="#777777"/>
+<path d="M12.2915 7.25563C12.5563 6.47835 13.6625 6.44651 13.9717 7.20729L14.0002 7.28566L15.5508 11.25H18C18.4142 11.25 18.75 11.5858 18.75 12C18.75 12.4142 18.4142 12.75 18 12.75H14.4485L14.2844 12.2234L13.2048 9.77077L10.9636 16.7378C10.7527 17.5814 9.58706 17.665 9.25781 16.8601L6.68115 11.5635L6.08862 12.75H3C2.58579 12.75 2.25 12.4142 2.25 12C2.25 11.5858 2.58579 11.25 3 11.25H5.16138L5.93115 9.71047L6.00366 9.58889C6.37321 9.06349 7.17791 9.09399 7.50659 9.64601L7.56958 9.77199L9.95215 14.5969L12.2681 7.3362L12.2915 7.25563Z" fill="#777777"/>
+</g>
+<defs>
+<clipPath id="clip0_1199_4516">
+<rect width="24" height="24" fill="white"/>
+</clipPath>
+</defs>
+</svg>`,k=n("EvaluateOutlined",P),R=`<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12.75 7.8809V19.7615L19.1624 17.9297C19.8062 17.7457 20.25 17.1565 20.25 16.4869V5.73856L12.75 7.8809ZM21.75 16.4869C21.75 17.8263 20.8619 19.0039 19.574 19.3719L12.2058 21.4768C11.7268 21.6136 11.25 21.2536 11.25 20.7554V7.31547C11.2501 6.98073 11.4723 6.68673 11.7942 6.59476L19.8376 4.29642C20.7958 4.02265 21.75 4.74203 21.75 5.73856V16.4869Z" fill="#0374E9"/>
+<path d="M11.25 7.8809V19.7615L4.83765 17.9297C4.19379 17.7457 3.75 17.1565 3.75 16.4869V5.73856L11.25 7.8809ZM2.25 16.4869C2.25 17.8263 3.13812 19.0039 4.42603 19.3719L11.7942 21.4768C12.2732 21.6136 12.75 21.2536 12.75 20.7554V7.31547C12.7499 6.98073 12.5277 6.68673 12.2058 6.59476L4.16235 4.29642C3.20415 4.02265 2.25004 4.74203 2.25 5.73856V16.4869Z" fill="#0374E9"/>
+</svg>`,T=n("KnowledgeOutlined",R),z=`<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="5" y="3" width="1.4" height="13" fill="#0374E9"/>
+<rect x="13.5" y="3" width="1.4" height="13" fill="#0374E9"/>
+</svg>`,A=n("PauseOutlined",z),K=`<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M15.9663 5.33471C16.6985 6.06694 16.6985 7.25413 15.9663 7.98636L8.01618 15.9365L4.47582 15.9413L4.48065 12.4009L12.4308 4.45083C13.163 3.71859 14.3502 3.71859 15.0824 4.45083L15.9663 5.33471ZM5.72983 12.9195L5.72775 14.6894L7.49759 14.6873L13.3147 8.87024L11.5469 7.10248L5.72983 12.9195ZM14.1985 5.33471C13.9545 5.09063 13.5587 5.09063 13.3147 5.33471L12.4308 6.21859L14.1985 7.98636L15.0824 7.10248C15.3265 6.8584 15.3265 6.46267 15.0824 6.21859L14.1985 5.33471Z" fill="#0374E9"/>
+</svg>`,U=n("SettingOutlined",K),W=`<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M10.4177 1.88574C13.1393 2.02366 15.5059 3.50144 16.8748 5.67188V4.375H18.1248V8.75H16.7595C16.1715 5.55005 13.3697 3.12501 9.99976 3.125C6.62989 3.12512 3.82792 5.55009 3.23999 8.75H1.97046C2.5717 4.85622 5.93764 1.87513 9.99976 1.875L10.4177 1.88574Z" fill="#0374E9"/>
+<path d="M9.67773 18.1143C6.95618 17.9763 4.58954 16.4986 3.2207 14.3281V15.625H1.9707V11.25H3.33594C3.92391 14.45 6.72576 16.875 10.0957 16.875C13.4656 16.8749 16.2675 14.4499 16.8555 11.25H18.125C17.5238 15.1438 14.1578 18.1249 10.0957 18.125L9.67773 18.1143Z" fill="#0374E9"/>
+</svg>`,q=n("SyncOutlined",W),F=`<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M11 18.5625C15.1767 18.5625 18.5625 15.1767 18.5625 11C18.5625 6.82335 15.1767 3.4375 11 3.4375C6.82335 3.4375 3.4375 6.82335 3.4375 11C3.4375 15.1767 6.82335 18.5625 11 18.5625ZM11 19.9375C15.936 19.9375 19.9375 15.936 19.9375 11C19.9375 6.06395 15.936 2.0625 11 2.0625C6.06395 2.0625 2.0625 6.06395 2.0625 11C2.0625 15.936 6.06395 19.9375 11 19.9375Z" fill="#777777"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M14.4028 7.59708L12.9444 12.9446L7.59689 14.403L9.0553 9.05548L14.4028 7.59708ZM12.4431 9.55675L10.1751 10.1753L9.55656 12.4433L11.8246 11.8248L12.4431 9.55675Z" fill="#777777"/>
+</svg>`,G=n("WorkbenchOutlined",F),h=Object.freeze(Object.defineProperty({__proto__:null,AngleLeftOutlined:B,CompassOutlined:b,DatasetOutlined:N,DeleteOutlined:o,DownloadOutlined:D,EditOutlined:$,EvaluateOutlined:k,KnowledgeOutlined:T,PauseOutlined:A,SettingOutlined:U,SyncOutlined:q,WorkbenchOutlined:G},Symbol.toStringTag,{value:"Module"})),Y={title:"@fe-free/icons",tags:["autodocs"]};function J({name:s,icon:e}){const[l,c]=L.useState(!1),i=async()=>{try{await H(s),c(!0)}catch(d){console.error("复制失败:",d)}};return t.jsxs("div",{className:"flex cursor-pointer flex-col items-center rounded border border-transparent p-2 hover:border-01",onClick:i,children:[t.jsx("div",{className:"relative",children:t.jsx(e,{className:"text-3xl"})}),t.jsx("span",{className:"text-sm",children:s}),t.jsx("span",{className:V("text-xs text-03",{invisible:!l,visible:l}),children:l?"已复制!":"点击复制"})]})}console.log(h);const r={render:()=>{const s=L.useMemo(()=>Object.entries(h).map(([e,l])=>({name:e,icon:l})).sort((e,l)=>e.name.localeCompare(l.name)),[]);return console.log(s),t.jsx("div",{className:"flex flex-wrap gap-4",children:s.map(({name:e,icon:l})=>t.jsx(J,{name:e,icon:l},e))})},parameters:{docs:{description:{story:"所有可用的图标列表，点击图标卡片可复制组件名称"}}}},a={render:()=>t.jsxs("div",{children:[t.jsx(o,{}),t.jsx(o,{className:"text-xl"}),t.jsx(o,{className:"text-primary"}),t.jsx(o,{rotate:45}),t.jsx(o,{spin:!0,className:"text-2xl"})]})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    // 自动获取所有导出的图标组件
+    const icons = useMemo(() => {
+      return Object.entries(Icons).map(([name, icon]) => ({
+        name,
+        icon: icon as typeof Icons.DeleteOutlined
+      })).sort((a, b) => a.name.localeCompare(b.name)); // 按名称排序
+    }, []);
+    console.log(icons);
+    return <div className="flex flex-wrap gap-4">
+        {icons.map(({
+        name,
+        icon
+      }) => <IconItem key={name} name={name} icon={icon} />)}
+      </div>;
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: '所有可用的图标列表，点击图标卡片可复制组件名称'
+      }
+    }
+  }
+}`,...r.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  render: () => <div>
+      <Icons.DeleteOutlined />
+      <Icons.DeleteOutlined className="text-xl" />
+      <Icons.DeleteOutlined className="text-primary" />
+      <Icons.DeleteOutlined rotate={45} />
+      <Icons.DeleteOutlined spin className="text-2xl" />
+    </div>
+}`,...a.parameters?.docs?.source}}};const e1=["Basic","Usages"];export{r as Basic,a as Usages,e1 as __namedExportsOrder,Y as default};
