80d3241b5d153d7efb5480ccbf38d2cb8133b265
liyatang <liyatang@users.noreply.github.com>
2026-01-06 01:40:01 +0000

    deploy: 220ab4c79348cb064f642e6be87a543e87fb1763

--- assets/percentage_slider.stories-aWl5CosZ.js
+++
@@ -1,4 +0,0 @@
-import{A as r,r as o,j as n}from"./iframe-BRsowCfw.js";import"./preload-helper-PPVm8Dsz.js";const m={title:"@fe-free/core/PercentageSlider",component:r,tags:["autodocs"]};function c(s){const[t,a]=o.useState([10,30,60]);return n.jsx(r,{value:t,onChange:a,...s})}const e={args:{},render:c};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {},
-  render: Render
-}`,...e.parameters?.docs?.source}}};const p=["Basic"];export{e as Basic,p as __namedExportsOrder,m as default};
