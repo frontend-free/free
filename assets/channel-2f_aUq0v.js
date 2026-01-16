44dd4b42800f8d55c450aa798bebc75f929da21c
liyatang <liyatang@users.noreply.github.com>
2026-01-16 06:59:40 +0000

    deploy: f1ce2194a2ab7d92d24e307b184701167aee0215

--- assets/channel-2f_aUq0v.js
+++
@@ -1,1 +0,0 @@
-import{U as a,G as n}from"./index-Dv2Zg2WW.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ assets/messages.stories-CFN9I9Uw.js
@@ -0,0 +1,25 @@
+import{j as t,C as n}from"./iframe-B4IOoIx6.js";import{b as o}from"./index-BF3T7Jcv.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-D1sIXqz8.js";import"./index-DrFu-skq.js";const m={title:"@fe-free/ai/MessageThink",component:o,tags:["autodocs"]},e={args:{title:"思考",children:"这是 Think 的内容"}},r={args:{title:"思考中...",loading:!0,children:"这是 Think 的内容"}},a={args:{title:"思考",children:"这是 Think 的内容",icon:t.jsx(n,{className:"text-green08"})}},s={args:{title:"思考中...",loading:!0,children:"这是 Think 的内容",type:"deepSeek"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: '思考',
+    children: '这是 Think 的内容'
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: '思考中...',
+    loading: true,
+    children: '这是 Think 的内容'
+  }
+}`,...r.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: '思考',
+    children: '这是 Think 的内容',
+    icon: <CheckCircleOutlined className="text-green08" />
+  }
+}`,...a.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: '思考中...',
+    loading: true,
+    children: '这是 Think 的内容',
+    type: 'deepSeek'
+  }
+}`,...s.parameters?.docs?.source}}};const g=["Default","Loading","Icon","DeepSeek"];export{s as DeepSeek,e as Default,a as Icon,r as Loading,g as __namedExportsOrder,m as default};
