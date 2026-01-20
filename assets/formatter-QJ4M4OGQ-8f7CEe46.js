f0678c51a06a4c20b7e437090520757a275c6aff
liyatang <liyatang@users.noreply.github.com>
2026-01-20 07:32:44 +0000

    deploy: f5a0cf3e50169fb06899eb850bd200cc9c20d1e5

---
+++ assets/formatter-QJ4M4OGQ-8f7CEe46.js
@@ -0,0 +1,1 @@
+import{d as m,r as a}from"./blocks-CgFoEs5G.js";import{aa as e}from"./iframe-DgQ-aEXn.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DrFu-skq.js";var o=e(a(),1),s=(0,o.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
