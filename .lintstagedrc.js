ec120ca362aaf8b19c2d530d9d24df25326d0c89
liyatang <liyatang@qq.com>
2026-03-02 21:54:11 +0800

    feat: oxlint oxfmt

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-  '*.{css,less,scss,sass}': ['prettier --write', 'stylelint --fix'],
-  '*.{md,mdx,json,html}': ['prettier --write'],
+  '*.{js,jsx,ts,tsx}': ['oxlint --fix', 'oxfmt'],
+  '*.{css,less,scss,sass}': ['oxfmt', 'stylelint --fix'],
+  '*.{md,mdx,json,html}': ['oxfmt'],
 };
