f9c1f408e43f7492837a415feac853d9fb167042
liyatang <liyatang@qq.com>
2026-03-10 16:36:01 +0800

    feat: tabs

--- packages/core/src/tabs/index.tsx
+++ packages/core/src/tabs/index.tsx
@@ -2,28 +2,26 @@ import type { TabsProps as AntdTabsProps } from 'antd';
 import { Tabs as AntdTabs } from 'antd';
 import classNames from 'classnames';
 import { useSearchParams } from 'react-router-dom';
+
 import { routeTool } from '../route';
+
 import './style.scss';
 
-interface TabsProps extends AntdTabsProps {
+interface TabsProps {
   /** 自动时设置和同步 searchParams tab */
   withSearchParams?: boolean;
   /** 设置 searchParams 的类型，默认 set */
   searchParamsType?: 'set' | 'change';
   /** 默认 tab */
   tabKey?: string;
+
+  tabProps?: AntdTabsProps;
 }
 
 function Tabs(props: TabsProps) {
-  const {
-    withSearchParams,
-    searchParamsType = 'set',
-    tabKey = 'tab',
-    activeKey,
-    onChange,
-    className,
-    ...rest
-  } = props;
+  const { withSearchParams, searchParamsType = 'set', tabKey = 'tab', tabProps } = props;
+  const { className, activeKey, onChange, ...rest } = tabProps || {};
+
   const [searchParams] = useSearchParams();
   const tab = searchParams.get(tabKey) || undefined;
 
