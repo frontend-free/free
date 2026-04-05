8abbc83e44ad01b1831aa411f0396f7009460d53
liyatang <liyatang@qq.com>
2026-04-05 10:36:58 +0800

    storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,8 +1,9 @@
 import { ProConfigProvider } from '@ant-design/pro-components';
 import { customValueTypeMap } from '@fe-free/core';
 import { themeConfig } from '@fe-free/core/src/core_app/config';
-import { EnumLanguage, I18nProvider } from '@fe-free/core/src/i18n';
+import { EnumLanguage, I18nProvider, initI18n } from '@fe-free/core/src/i18n';
 
+// @ts-ignore
 import '@fe-free/core/src/tailwind.css';
 import type { Preview } from '@storybook/react-vite';
 import { App, ConfigProvider } from 'antd';
@@ -12,6 +13,8 @@ import i18n from 'i18next';
 import React from 'react';
 
 console.log('i18n.language', i18n.language);
+
+initI18n({});
 
 const preview: Preview = {
   parameters: {
